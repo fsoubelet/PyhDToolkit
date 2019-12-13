e6b2cb902aef2769cab2777e9902cd300b83bd05
Felix Soubelet <felix.soubelet@protonmail.com>
2019-12-13 13:00:44 +0100

    Add executors utility
    The __init__.py files have been modified, I'm still not sure of the imports that should be highest-level.
    Might change my mind on it later.

--- pyhdtoolkit/utils/__init__.py
+++ pyhdtoolkit/utils/__init__.py
@@ -1,3 +1,4 @@
+from .executors import MultiProcessor, MultiThreader
 from .printutil import END, Background, Foreground, Styles
 from .structures import DotDict
 from .struct_operations import ListOperations, MiscellaneousOperations, NumberOperations, StringOperations
