bc3fdf62079edf41253162ad70fab9d9e91b19bb
Felix Soubelet <felix.soubelet@protonmail.com>
2020-02-06 12:11:18 +0100

    dump

--- pyhdtoolkit/omc_math/nonconvex_phase_sync.py
+++ pyhdtoolkit/omc_math/nonconvex_phase_sync.py
@@ -44,9 +44,11 @@ See the `walkthrough.md` file to have an overview of how to use this API.
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.stats as st
 import tfs
 
 from pyhdtoolkit.utils.cmdline import CommandLine
+from pyhdtoolkit.utils.printutil import Foreground as Fg, END as E
 
 
 class PhaseReconstructor:
@@ -290,13 +292,99 @@ def create_2d_gamma_noise(dist_shape: int, scale: float, size: tuple) -> np.ndar
     return upper_triangle - upper_triangle.T
 
 
+def best_fit_distribution(distribution_data: pd.Series, tested_distributions: dict = None, bins: int = None, ax=None):
+    """
+    Model the provided data by finding best fit distribution to said data.
+    Will do a fit
+    """
+    # Get histogram of original data
+    bins = 150 if bins is None else bins
+    tested_distributions = (
+        {
+            st.chi: "Chi",
+            st.chi2: "ChiSquared",
+            st.expon: "Exponential",
+            st.gamma: "Gamma",
+            st.laplace: "Laplace",
+            st.lognorm: "LogNorm",
+            st.norm: "Normal",
+        }
+        if tested_distributions is None
+        else tested_distributions
+    )
+
+    # Preparing data
+    y, x = np.histogram(distribution_data, bins=bins, density=True)
+    x = (x + np.roll(x, -1))[:-1] / 2.0
+
+    # Best holders
+    best_distribution = st.norm
+    best_params = (0.0, 1.0)
+    best_sse = np.inf
+
+    # Estimate distribution parameters from data
+    for distribution, distname in tested_distributions.items():
+        print(f"Trying fit for {Fg.dark_red}{distname}{E} distribution.")
+
+        try:  # Try to fit the distribution
+            with warnings.catch_warnings():  # Ignore warnings from data that can't be fit
+                warnings.filterwarnings("ignore")
+
+                # fit dist to data and separate parts of parameters
+                params = distribution.fit(distribution_data)
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+
+                # Calculate fitted PDF and error with fit in distribution
+                pdf = distribution.pdf(x, loc=loc, scale=scale, *arg)
+                sse = np.sum(np.power(y - pdf, 2.0))
+
+                try:
+                    # if ax:
+                    pd.Series(pdf, x).plot(ax=ax, label=f"{distname} fit", alpha=1, lw=2)
+                except Exception:
+                    pass
+
+                # identify if this distribution is better
+                if best_sse > sse > 0:
+                    best_distribution = distribution
+                    best_params = params
+                    best_sse = sse
+
+        except Exception:
+            pass
+
+    return best_distribution.name, best_params
+
+
 def get_combinations_matrices_from_madx(madx_file: str) -> (np.ndarray, np.ndarray):
     """Call the madx file, analyse twiss and give back combinations."""
     cmd = CommandLine()
-    cmd.run("madx twiss.LHC.BPMs.madx")
+    cmd.run(f"madx {madx_file}")
     comb_mat_x, comb_mat_y = create_combinations_matrices_from_twiss("bpms.tfs")
     cmd.run("rm -f bpms.tfs")
     return comb_mat_x, comb_mat_y
+
+
+def make_pdf_from_stats_distribution(dist, params, size=10000):
+    """
+    Generate distributions's Probability Distribution Function.
+    """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    # Get sane start and end points of distribution
+    start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+    end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    y = dist.pdf(x, loc=loc, scale=scale, *arg)
+    return pd.Series(y, x)
 
 
 def meas_noise_matrix_to_dataframe(matrix: np.ndarray) -> pd.DataFrame:
