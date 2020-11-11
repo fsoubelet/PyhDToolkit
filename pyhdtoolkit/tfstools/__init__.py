6b65b5faafe523c954bda842fbfe07facdbfa95b
Felix Soubelet <felix.soubelet@protonmail.com>
2020-11-11 21:36:22 +0100

    latwiss in tfstools

---
+++ pyhdtoolkit/tfstools/__init__.py
@@ -0,0 +1,11 @@
+"""
+tfstools package
+~~~~~~~~~~~~~~~~~~~
+tfstools is a collection of utilities that integrate within my workflow when manipulating `tfs`
+files.
+
+:copyright: (c) 2019 by Felix Soubelet.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .latwiss import LaTwiss
