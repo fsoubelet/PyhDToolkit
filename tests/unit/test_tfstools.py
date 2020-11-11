a2e438383488c19d8a232a826e8bf0122c8d6f87
Felix Soubelet <felix.soubelet@protonmail.com>
2020-11-11 22:36:23 +0100

    tests for tfstools now ok

--- tests/unit/test_tfstools.py
+++ tests/unit/test_tfstools.py
@@ -10,7 +10,7 @@ from cpymad.madx import Madx
 
 from pyhdtoolkit.cpymadtools.helpers import LatticeMatcher
 from pyhdtoolkit.cpymadtools.lattice_generators import LatticeGenerator
-from pyhdtoolkit.tfstools.latwiss import LaTwiss
+from pyhdtoolkit.tfstools.latwiss import LaTwiss, _assert_necessary_columns
 
 # Forcing non-interactive Agg backend so rendering is done similarly across platforms during tests
 matplotlib.use("Agg")
@@ -21,7 +21,7 @@ BASE_LATTICE = LatticeGenerator.generate_base_cas_lattice()
 
 class TestLaTwiss:
     @pytest.mark.mpl_image_compare(tolerance=20, style="seaborn-pastel")
-    def test_plot_latwiss_from_frame(self, tmp_path):
+    def test_plot_latwiss_from_pandas_frame(self, tmp_path):
         """Using my CAS 19 project's base lattice."""
         savefig_dir = tmp_path / "test_plot_latwiss"
         savefig_dir.mkdir()
@@ -45,14 +45,14 @@ class TestLaTwiss:
         return figure
 
     @pytest.mark.mpl_image_compare(tolerance=20, style="seaborn-pastel")
-    def test_plot_latwiss_from_tfs_file(self, tmp_path, _latwiss_tfs):
+    def test_plot_latwiss_from_tfs_frame(self, tmp_path, _latwiss_tfs_frame):
         """Using my CAS 19 project's base lattice."""
         savefig_dir = tmp_path / "test_plot_latwiss"
         savefig_dir.mkdir()
         saved_fig = savefig_dir / "latwiss.png"
 
         figure = LaTwiss.plot_latwiss(
-            twiss_ouptut=_latwiss_tfs,
+            twiss_ouptut=_latwiss_tfs_frame,
             title="Project 3 Base Lattice",
             xlimits=(-50, 1_050),
             beta_ylim=(5, 75),
@@ -62,8 +62,57 @@ class TestLaTwiss:
         assert saved_fig.is_file()
         return figure
 
+    @pytest.mark.mpl_image_compare(tolerance=20, style="seaborn-pastel")
+    def test_plot_latwiss_from_file(self, tmp_path, _latwiss_tfs_path):
+        """Using my CAS 19 project's base lattice."""
+        savefig_dir = tmp_path / "test_plot_latwiss"
+        savefig_dir.mkdir()
+        saved_fig = savefig_dir / "latwiss.png"
+
+        figure = LaTwiss.plot_latwiss(
+            twiss_ouptut=_latwiss_tfs_path,
+            title="Project 3 Base Lattice",
+            xlimits=(-50, 1_050),
+            beta_ylim=(5, 75),
+            plot_sextupoles=True,
+            savefig=saved_fig,
+        )
+        assert saved_fig.is_file()
+        return figure
+
+    def test_plot_latwiss_crash_on_wrong_datatype(self, caplog):
+        wrong_data = np.array([1, 2, 3, 4, 5])
+
+        with pytest.raises(ValueError):
+            _ = LaTwiss.plot_latwiss(twiss_ouptut=wrong_data, title="Project 3 Base Lattice",)
+
+        for record in caplog.records:
+            assert record.levelname == "ERROR"
+
+
+def test_assert_columns_fails_on_absent_column(caplog):
+    madx = Madx(stdout=False)
+    madx.input(BASE_LATTICE)
+    LatticeMatcher.perform_tune_matching(madx, "CAS3", 6.335, 6.29)
+    LatticeMatcher.perform_chroma_matching(madx, "CAS3", 100, 100)
+    twiss = madx.table.twiss.dframe()
+
+    with pytest.raises(KeyError):
+        _assert_necessary_columns(twiss, columns=["some_obviously_absent_colname"])
+
+    for record in caplog.records:
+        assert record.levelname == "ERROR"
+
+
+# ----- Fixtures -----
+
 
 @pytest.fixture()
-def _latwiss_tfs() -> tfs.TfsDataFrame:
+def _latwiss_tfs_path() -> pathlib.Path:
+    return pathlib.Path(__file__).parent.parent / "inputs" / "tfstools_latwiss.tfs"
+
+
+@pytest.fixture()
+def _latwiss_tfs_frame() -> tfs.TfsDataFrame:
     tfs_file_path = pathlib.Path(__file__).parent.parent / "inputs" / "tfstools_latwiss.tfs"
-    return tfs.read(tfs_file_path, index="NAME")
+    return tfs.read(tfs_file_path)
