fd2e08d757963026ef81483b6846e97f76a716b7
Felix Soubelet <felix.soubelet@protonmail.com>
2021-02-12 21:13:42 +0100

    remove scripts and unused deps, update lock file

--- pyhdtoolkit/scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-scripts module
-~~~~~~~~~~~~~~~
-Various scripts to run, that will help manipulate data or run different flow of programs and
-simulations. This is helpful in my work.
-
-:copyright: (c) 2019-2020 by Felix Soubelet.
-:license: MIT, see LICENSE for more details.
-"""
