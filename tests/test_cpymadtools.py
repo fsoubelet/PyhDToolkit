ec5c9d36ba5a81bbf8c27eb108f3d0fa856478b4
Felix Soubelet <felix.soubelet@protonmail.com>
2021-04-19 15:00:47 +0200

    adapted tests for new errors functions, coverage 100%

--- tests/test_cpymadtools.py
+++ tests/test_cpymadtools.py
@@ -21,7 +21,11 @@ from pyhdtoolkit.cpymadtools.constants import (  # for coverage
     SPECIAL_FAMILIES,
     TWO_FAMILIES,
 )
-from pyhdtoolkit.cpymadtools.errors import misalign_lhc_triplets, switch_magnetic_errors
+from pyhdtoolkit.cpymadtools.errors import (
+    misalign_lhc_ir_quadrupoles,
+    misalign_lhc_triplets,
+    switch_magnetic_errors,
+)
 from pyhdtoolkit.cpymadtools.generators import LatticeGenerator
 from pyhdtoolkit.cpymadtools.latwiss import plot_latwiss, plot_machine_survey
 from pyhdtoolkit.cpymadtools.matching import (
@@ -136,34 +140,61 @@ class TestErrors:
 
     @pytest.mark.parametrize("ip", [1, 2, 5, 8])
     @pytest.mark.parametrize("sides", ["R", "L", "RL", "r", "l", "rl"])
-    def test_misalign_triplets(self, _non_matched_lhc_madx, ip, sides):
+    @pytest.mark.parametrize("quadrupoles", [[1, 3, 5, 7, 9], list(range(1, 11))])
+    def test_misalign_lhc_ir_quadrupoles(self, _non_matched_lhc_madx, ip, sides, quadrupoles):
         madx = _non_matched_lhc_madx
-        misalign_lhc_triplets(madx, ip=ip, sides=sides, dx="1E-3 * TGAUSS(2.5)", dpsi="1E-3 * TGAUSS(2.5)")
-        error_table = madx.table["triplet_errors"].dframe().copy()
+        misalign_lhc_ir_quadrupoles(
+            madx,
+            ip=ip,
+            quadrupoles=quadrupoles,
+            beam=1,
+            sides=sides,
+            dx="1E-3 * TGAUSS(2.5)",
+            dpsi="1E-3 * TGAUSS(2.5)",
+        )
+        error_table = madx.table["ir_quads_errors"].dframe().copy()
         assert all(error_table["dx"] != 0)
         assert all(error_table["dpsi"] != 0)
 
-    def test_misalign_triplets_specific_value(self, _non_matched_lhc_madx):
+    def test_misalign_lhc_ir_quadrupoles_specific_value(self, _non_matched_lhc_madx):
         madx = _non_matched_lhc_madx
-        misalign_lhc_triplets(madx, ip=1, sides="RL", dy="0.001")
-        error_table = madx.table["triplet_errors"].dframe().copy()
+        misalign_lhc_ir_quadrupoles(
+            madx, ip=1, quadrupoles=list(range(1, 11)), beam=1, sides="RL", dy="0.001"
+        )
+        error_table = madx.table["ir_quads_errors"].dframe().copy()
         assert all(error_table["dy"] == 0.001)
 
-    def test_misalign_triplets_raises_on_wrong_side(self, _non_matched_lhc_madx, caplog):
+    def test_misalign_lhc_ir_quadrupoles_raises_on_wrong_side(self, _non_matched_lhc_madx, caplog):
         madx = _non_matched_lhc_madx
         with pytest.raises(ValueError):
-            misalign_lhc_triplets(madx, ip=1, sides="Z")
+            misalign_lhc_ir_quadrupoles(madx, ip=8, quadrupoles=[1], beam=2, sides="Z", dy="0.001")
 
         for record in caplog.records:
             assert record.levelname == "ERROR"
 
-    def test_misalign_triplets_raises_on_wrong_ip(self, _non_matched_lhc_madx, caplog):
+    def test_misalign_lhc_ir_quadrupoles_raises_on_wrong_ip(self, _non_matched_lhc_madx, caplog):
         madx = _non_matched_lhc_madx
         with pytest.raises(ValueError):
-            misalign_lhc_triplets(madx, ip=100, sides="R")
+            misalign_lhc_ir_quadrupoles(madx, ip=100, quadrupoles=[1], beam=2, sides="R", dy="0.001")
 
         for record in caplog.records:
             assert record.levelname == "ERROR"
+
+    def test_misalign_lhc_ir_quadrupoles_raises_on_wrong_beam(self, _non_matched_lhc_madx, caplog):
+        madx = _non_matched_lhc_madx
+        with pytest.raises(ValueError):
+            misalign_lhc_ir_quadrupoles(madx, ip=2, quadrupoles=[1], beam=10, sides="L", dy="0.001")
+
+        for record in caplog.records:
+            assert record.levelname == "ERROR"
+
+    def test_misalign_lhc_triplets(self, _non_matched_lhc_madx):
+        # for coverage as this calls `misalign_lhc_ir_quadrupoles` tested above
+        madx = _non_matched_lhc_madx
+        misalign_lhc_triplets(madx, ip=1, sides="RL", dx="1E-3 * TGAUSS(2.5)", dpsi="1E-3 * TGAUSS(2.5)")
+        error_table = madx.table["triplet_errors"].dframe().copy()
+        assert all(error_table["dx"] != 0)
+        assert all(error_table["dpsi"] != 0)
 
 
 class TestLatticeGenerator:
