a4793a7b28d9985a36a233b350bc5041c176eefe
Felix Soubelet <19598248+fsoubelet@users.noreply.github.com>
2021-06-28 17:11:18 +0000

    Tracking improvements (#43)

--- tests/test_cpymadtools.py
+++ tests/test_cpymadtools.py
@@ -829,10 +829,11 @@ class TestSpecial:
         madx = _matched_lhc_madx
         make_lhc_thin(madx, sequence="lhcb1", slicefactor=4)
 
-        tracks = track_single_particle(
+        tracks_dict = track_single_particle(
             madx, initial_coordinates=(1e-4, 0, 1e-4, 0, 0, 0), nturns=10, sequence="lhcb1"
         )
-        assert isinstance(tracks, DataFrame)
+        assert isinstance(tracks_dict, dict)
+        tracks = tracks_dict["observation_point_1"]
         assert len(tracks) == 11  # nturns + 1 because $start coordinates also given by MAD-X
         assert all(
             [coordinate in tracks.columns for coordinate in ("x", "px", "y", "py", "t", "pt", "s", "e")]
@@ -880,14 +881,36 @@ class TestSpecial:
 
 
 class TestTrack:
-    def test_single_particle_tracking(self, _matched_base_lattice):
+    @pytest.mark.parametrize("obs_points", [[], ["qf", "mb", "msf"]])
+    def test_single_particle_tracking(self, _matched_base_lattice, obs_points):
         madx = _matched_base_lattice
-        tracks = track_single_particle(
-            madx, initial_coordinates=(1e-4, 0, 2e-4, 0, 0, 0), nturns=100, sequence="CAS3"
+        tracks_dict = track_single_particle(
+            madx,
+            sequence="CAS3",
+            nturns=100,
+            initial_coordinates=(1e-4, 0, 2e-4, 0, 0, 0),
+            observation_points=obs_points,
         )
 
+        assert isinstance(tracks_dict, dict)
+        assert len(tracks_dict.keys()) == len(obs_points) + 1
+        for tracks in tracks_dict.values():
+            assert isinstance(tracks, DataFrame)
+            assert all(
+                [coordinate in tracks.columns for coordinate in ("x", "px", "y", "py", "t", "pt", "s", "e")]
+            )
+
+    def test_single_particle_tracking_with_onepass(self, _matched_base_lattice):
+        madx = _matched_base_lattice
+        tracks_dict = track_single_particle(
+            madx, sequence="CAS3", nturns=100, initial_coordinates=(2e-4, 0, 1e-4, 0, 0, 0), ONETABLE=True,
+        )
+
+        assert isinstance(tracks_dict, dict)
+        assert len(tracks_dict.keys()) == 1  # should be only one because of ONETABLE option
+        assert "trackone" in tracks_dict.keys()
+        tracks = tracks_dict["trackone"]
         assert isinstance(tracks, DataFrame)
-        assert len(tracks) == 101  # nturns + 1 because $start coordinates also given by MAD-X
         assert all(
             [coordinate in tracks.columns for coordinate in ("x", "px", "y", "py", "t", "pt", "s", "e")]
         )
