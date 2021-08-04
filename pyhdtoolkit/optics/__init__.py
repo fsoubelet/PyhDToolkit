b116e09256b12e889f96269a703e7d89146a49af
Felix Soubelet <felix.soubelet@protonmail.com>
2021-08-04 12:34:21 +0200

    proper exports

--- pyhdtoolkit/optics/__init__.py
+++ pyhdtoolkit/optics/__init__.py
@@ -7,4 +7,6 @@ These are miscellaneous utilities to perform optics calculation from simulation 
 :license: MIT, see LICENSE for more details.
 """
 
-from .beam import Beam
+from .beam import Beam, compute_beam_parameters
+from .ripken import lebedev_beam_size
+from .twiss import courant_snyder_transform
