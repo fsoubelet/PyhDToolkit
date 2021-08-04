c06e0102fd80097d9a27c09779905973216edb90
Felix Soubelet <felix.soubelet@protonmail.com>
2021-08-04 12:32:31 +0200

    MADXBeam belongs to models.madx

--- pyhdtoolkit/models/beam.py
+++ pyhdtoolkit/models/beam.py
@@ -9,35 +9,7 @@ A module with `pydantic` models to validate and store data structures used in th
 """
 from typing import Optional
 
-from pydantic import BaseModel, PositiveInt
-
-
-class MADXBeam(BaseModel):
-    """
-    This is a class to encompass and manipulate
-    """
-
-    particle: str
-    mass: float
-    charge: float
-    energy: float  # Total energy per particle in [GeV]
-    pc: float  # Particle momentum times the speed of light, in [GeV]
-    gamma: float  # Relativistic factor in [1]
-    beta: float  # Relativistic beta in [1]
-    brho: float  # Magnetic rigidity of the particles in [T.m]
-    ex: float  # Horizontal emittance in [m]
-    ey: float  # Vertical emittance in [m]
-    et: float  # Longitudinal emittance in [m]
-    exn: float  # Normalized horizontal emittance in [m] (beta * gamma * ex)
-    eyn: float  # Normalized vertical emittance in [m]  *beta * gamma * ey)
-    sigt: float  # The bunch length c σt in [m]
-    sige: float  # The relative energy spread σE /E in [1].
-    kbunch: PositiveInt  # The number of particle bunches in the machine in [1]
-    npart: PositiveInt  # The number of particles per bunch in [1]
-    bcurrent: float  # The bunch current, in [A]
-    bunched: bool  # A logical flag. If set, the beam is treated as bunched whenever this makes sense
-    radiate: bool  # A logical flag. If set, synchrotron radiation is considered in all dipole magnets
-    bv: int  # integer specifying the direction of the particle movement in the beam line; either +1 or -1
+from pydantic import BaseModel
 
 
 class BeamParameters(BaseModel):
