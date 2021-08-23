61edf061a9e553723b119bd6efc6f357784d6c06
Felix Soubelet <felix.soubelet@protonmail.com>
2021-08-23 16:34:01 +0200

    properly install ACD for no negative drift and USE sequence after installation. Can also choose beam 1 or 2

--- pyhdtoolkit/cpymadtools/special.py
+++ pyhdtoolkit/cpymadtools/special.py
@@ -203,15 +203,16 @@ def install_ac_dipole(
     deltaqy: float,
     sigma_x: float,
     sigma_y: float,
+    beam: int = 1,
     geometric_emit: float = None,
     start_turn: int = 100,
     ramp_turns: int = 2000,
     top_turns: int = 6600,
 ) -> None:
     """
-    Installs an AC dipole for BEAM 1 ONLY.
-    This function assumes that you have already defined lhcb1, made a beam for it (BEAM command or
-    `make_lhc_beams` function) and matched to your desired working point.
+    Installs an AC dipole for (HL)LHC BEAM 1 OR 2 ONLY.
+    This function assumes that you have already defined lhcb1/lhcb2, made a beam for it (BEAM command or
+    `make_lhc_beams` function), matched to your desired working point and made a TWISS.
 
     Args:
         madx (cpymad.madx.Madx): an instanciated cpymad Madx object.
@@ -219,6 +220,7 @@ def install_ac_dipole(
         deltaqy (float): the deltaQy (vertical tune excitation) used by the AC dipole.
         sigma_x (float): the horizontal amplitude to drive the beam to, in bunch sigma.
         sigma_y (float): the vertical amplitude to drive the beam to, in bunch sigma.
+        beam (int): the LHC beam to install the AC Dipole into, either 1 or 2. Defaults to 1.
         geometric_emit (float): the geometric emittance that was used when defining the beam. If not
             provided, it is assumed that 'geometric_emit' is a defined global in MAD-X, and the value will
             be directly queried from the internal tables.
@@ -228,6 +230,10 @@ def install_ac_dipole(
             as in the LHC.
         top_turns (int): the number of turns to drive the beam for. Defaults to 6600 as in the LHC.
     """
+    logger.warning(
+        "This AC Dipole installation routine should be done after 'match', 'twiss' and 'makethin' "
+        "for the appropriate beam."
+    )
     if top_turns > 6600:
         logger.warning(
             f"Configuring the AC Dipole for {top_turns} of driving is fine for MAD-X but is "
@@ -251,22 +257,26 @@ def install_ac_dipole(
 
     logger.info(f"Installing AC Dipole to drive the tunes to Qx_D = {q1_dipole}  |  Qy_D = {q2_dipole}")
     madx.input(
-        f"MKACH.6L4.B1: hacdipole, l=0, freq:={q1_dipole}, lag=0, volt:=voltx, ramp1={ramp1}, "
+        f"MKACH.6L4.B{beam:d}: hacdipole, l=0, freq:={q1_dipole}, lag=0, volt:=voltx, ramp1={ramp1}, "
         f"ramp2={ramp2}, ramp3={ramp3}, ramp4={ramp4};"
     )
     madx.input(
-        f"MKACV.6L4.B1: vacdipole, l=0, freq:={q2_dipole}, lag=0, volt:=volty, ramp1={ramp1}, "
+        f"MKACV.6L4.B{beam:d}: vacdipole, l=0, freq:={q2_dipole}, lag=0, volt:=volty, ramp1={ramp1}, "
         f"ramp2={ramp2}, ramp3={ramp3}, ramp4={ramp4};"
     )
-    madx.command.seqedit(sequence="lhcb1")
+    madx.command.seqedit(sequence=f"lhcb{beam:d}")
     madx.command.flatten()
-    madx.command.install(element="MKACH.6L4.B1", at="0.0", from_="MKQA.6L4.B1")
-    madx.command.install(element="MKACV.6L4.B1", at="0.0", from_="MKQA.6L4.B1")
+    madx.command.install(  # same position as in model_creator macros
+        element=f"MKACH.6L4.B{beam:d}", at="1.583 / 2", from_=f"MKQA.6L4.B{beam:d}"
+    )
+    madx.command.install(  # same position as in model_creator macros
+        element=f"MKACV.6L4.B{beam:d}", at="1.583 / 2", from_=f"MKQA.6L4.B{beam:d}"
+    )
     madx.command.endedit()
 
     logger.trace("Querying BETX and BETY at AC Dipole location")
-    madx.input("betax_acd = table(twiss, MKQA.6L4.B1, betx);")
-    madx.input("betay_acd = table(twiss, MKQA.6L4.B1, bety);")
+    madx.input(f"betax_acd = table(twiss, MKQA.6L4.B{beam:d}, betx);")
+    madx.input(f"betay_acd = table(twiss, MKQA.6L4.B{beam:d}, bety);")
 
     betax_acd = madx.globals["betax_acd"]
     betay_acd = madx.globals["betay_acd"]
@@ -275,6 +285,12 @@ def install_ac_dipole(
     volty = sigma_y * np.sqrt(geometric_emit) * brho * np.abs(deltaqy) * 4 * np.pi / np.sqrt(betay_acd)
     madx.globals["voltx"] = voltx
     madx.globals["volty"] = volty
+
+    logger.warning(
+        f"Sequence LHCB{beam:d} is now re-used for changes to take effect. Beware that this will reset it "
+        "to its default state, remove errors etc."
+    )
+    madx.use(sequence=f"lhcb{beam:d}")
 
 
 def vary_independent_ir_quadrupoles(
