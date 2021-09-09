eeed1bb29a8c93bd0d14fb4a430f4070af164406
Felix Soubelet <felix.soubelet@protonmail.com>
2021-09-09 11:54:17 +0200

    removed comments

--- pyhdtoolkit/models/beam.py
+++ pyhdtoolkit/models/beam.py
@@ -17,19 +17,12 @@ class BeamParameters(BaseModel):
     """
     Class to encompass, validate and manipulate properties of a particle beam.
     """
-    #TODO: removove comments
+
     pc_GeV: Optional[float]  # Beam momentum [GeV]
-    # B_rho_Tm: Optional[float] = self.pc_GeV / 0.3  # Beam rigidity [T/m]
     E_0_GeV: Optional[float] = 0.9382720813  # Particle rest mass energy [GeV], defaults to that of a proton
     charge: Optional[float] = 1  # Particle charge in [e], defaults to that of a proton
-    # E_tot_GeV: Optional[float]  # Total beam energy [GeV]
-    # E_kin_GeV: Optional[float]  # Kinectic beam energy [GeV]
-    # gamma_r: Optional[float]  # Relativistic gamma
-    # beta_r: Optional[float]  # Relativistic beta
     en_x_m: Optional[float]  # Horizontal normalized emittance [m]
     en_y_m: Optional[float]  # Vertical normalized emittance [m]
-    # eg_x_m: Optional[float]  # Horizontal geometrical emittance
-    # eg_y_m: Optional[float]  # Vertical geometrical emittance
     deltap_p: Optional[float]  # Momentum deviation
 
     @property
