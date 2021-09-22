e9a65daa20475ef0faaaa67b298731caf0fbeddd
Felix Soubelet <felix.soubelet@protonmail.com>
2021-09-22 10:08:43 +0200

    tests for aperture plotter

--- tests/test_cpymadtools/test_plotters.py
+++ tests/test_cpymadtools/test_plotters.py
@@ -10,6 +10,7 @@ from cpymad.madx import Madx
 from pyhdtoolkit.cpymadtools.generators import LatticeGenerator
 from pyhdtoolkit.cpymadtools.matching import match_tunes_and_chromaticities
 from pyhdtoolkit.cpymadtools.plotters import (
+    AperturePlotter,
     BeamEnvelopePlotter,
     CrossingSchemePlotter,
     DynamicAperturePlotter,
@@ -25,6 +26,59 @@ CURRENT_DIR = pathlib.Path(__file__).parent
 INPUTS_DIR = CURRENT_DIR.parent / "inputs"
 GUIDO_LATTICE = INPUTS_DIR / "guido_lattice.madx"
 BASE_LATTICE = LatticeGenerator.generate_base_cas_lattice()
+
+
+class TestAperturePlotter:
+    @pytest.mark.mpl_image_compare(tolerance=20, style="seaborn-pastel", savefig_kwargs={"dpi": 200})
+    def test_plot_aperture_cell_injection(self, tmp_path, _injection_aperture_tolerances_lhc_madx):
+        savefig_dir = tmp_path / "test_plot_envelope"
+        savefig_dir.mkdir()
+        saved_fig = savefig_dir / "aperture.png"
+
+        madx = _injection_aperture_tolerances_lhc_madx
+        madx.command.twiss(centre=True)
+        twiss_df = madx.table.twiss.dframe().copy()
+        ip5s = twiss_df.s[twiss_df.name.str.contains("ip5")].to_numpy()[0]
+        figure = AperturePlotter.plot_aperture(
+            madx,
+            title="Arc 56 Cell, Injection Optics Aperture Tolerance",
+            plot_bpms=True,
+            xlimits=(14_084.5, 14_191.3),  # cell somewhere in arc 56
+            aperture_ylim=(0, 25),
+            k0l_lim=(-3e-2, 3e-2),
+            k1l_lim=(-4e-2, 4e-2),
+            k2l_lim=(-5e-2, 5e-2),
+            color="brown",
+            savefig=saved_fig,
+        )
+
+        assert saved_fig.is_file()
+        return figure
+
+    @pytest.mark.mpl_image_compare(tolerance=20, style="seaborn-pastel", savefig_kwargs={"dpi": 200})
+    def test_plot_aperture_ir5_collision(self, tmp_path, _collision_aperture_tolerances_lhc_madx):
+        savefig_dir = tmp_path / "test_plot_envelope"
+        savefig_dir.mkdir()
+        saved_fig = savefig_dir / "aperture.png"
+
+        madx = _collision_aperture_tolerances_lhc_madx
+        madx.command.twiss(centre=True)
+        twiss_df = madx.table.twiss.dframe().copy()
+        ip5s = twiss_df.s[twiss_df.name.str.contains("ip5")].to_numpy()[0]
+
+        figure = AperturePlotter.plot_aperture(
+            madx,
+            title="IR 5, Injection Optics Aperture Tolerance",
+            plot_bpms=True,
+            xlimits=(ip5s - 80, ip5s + 80),
+            aperture_ylim=(0, 25),
+            k0l_lim=(-4e-4, 4e-4),
+            color="brown",
+            savefig=saved_fig,
+        )
+
+        assert saved_fig.is_file()
+        return figure
 
 
 class TestBeamEnvelopePlotter:
