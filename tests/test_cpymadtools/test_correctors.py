a0b3d9d9a3bae825e9f9134dfaaceb6262d0254b
Felix Soubelet <felix.soubelet@protonmail.com>
2021-10-18 11:53:30 +0200

    make into test class

--- tests/test_cpymadtools/test_correctors.py
+++ tests/test_cpymadtools/test_correctors.py
@@ -37,41 +37,39 @@ ALL_ARC_CORRECTOR_KNOBS = (
 )
 
 
-def test_query_undefined_triplet_corrector_knobs(_bare_lhc_madx):
-    madx = _bare_lhc_madx
-    make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
-    triplet_knobs = query_triplet_correctors_powering(madx)
-    assert all(knob in triplet_knobs for knob in ALL_TRIPLET_CORRECTOR_KNOBS)
-    assert all(knob_value == 0 for knob_value in triplet_knobs.values())  # as none were set
+class TestCorrectors:
+    def test_query_undefined_triplet_corrector_knobs(self, _bare_lhc_madx):
+        madx = _bare_lhc_madx
+        make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
+        triplet_knobs = query_triplet_correctors_powering(madx)
+        assert all(knob in triplet_knobs for knob in ALL_TRIPLET_CORRECTOR_KNOBS)
+        assert all(knob_value == 0 for knob_value in triplet_knobs.values())  # as none were set
 
+    def test_query_defined_triplet_corrector_knobs(self, _bare_lhc_madx):
+        madx = _bare_lhc_madx
+        make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
+        fake_knob_values = {knob: random.random() for knob in ALL_TRIPLET_CORRECTOR_KNOBS}
+        with madx.batch():
+            madx.globals.update(fake_knob_values)
+        triplet_knobs = query_triplet_correctors_powering(madx)
 
-def test_query_defined_triplet_corrector_knobs(_bare_lhc_madx):
-    madx = _bare_lhc_madx
-    make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
-    fake_knob_values = {knob: random.random() for knob in ALL_TRIPLET_CORRECTOR_KNOBS}
-    with madx.batch():
-        madx.globals.update(fake_knob_values)
-    triplet_knobs = query_triplet_correctors_powering(madx)
+        assert all(knob in triplet_knobs for knob in ALL_TRIPLET_CORRECTOR_KNOBS)
+        assert all(knob_value != 0 for knob_value in triplet_knobs.values())
 
-    assert all(knob in triplet_knobs for knob in ALL_TRIPLET_CORRECTOR_KNOBS)
-    assert all(knob_value != 0 for knob_value in triplet_knobs.values())
+    def test_query_undefined_arc_corrector_knobs(self, _bare_lhc_madx):
+        madx = _bare_lhc_madx
+        make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
+        arc_knobs = query_arc_correctors_powering(madx)
+        assert all(knob in arc_knobs for knob in ALL_ARC_CORRECTOR_KNOBS)
+        assert all(abs(knob_value) < 115 for knob_value in arc_knobs.values())  # set in opticsfile
 
+    def test_query_defined_arc_corrector_knobs(self, _bare_lhc_madx):
+        madx = _bare_lhc_madx
+        make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
+        fake_knob_values = {knob: random.random() for knob in ALL_ARC_CORRECTOR_KNOBS}
+        with madx.batch():
+            madx.globals.update(fake_knob_values)
+        arc_knobs = query_arc_correctors_powering(madx)
 
-def test_query_undefined_arc_corrector_knobs(_bare_lhc_madx):
-    madx = _bare_lhc_madx
-    make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
-    arc_knobs = query_arc_correctors_powering(madx)
-    assert all(knob in arc_knobs for knob in ALL_ARC_CORRECTOR_KNOBS)
-    assert all(abs(knob_value) < 115 for knob_value in arc_knobs.values())  # set in opticsfile
-
-
-def test_query_defined_arc_corrector_knobs(_bare_lhc_madx):
-    madx = _bare_lhc_madx
-    make_lhc_beams(madx)  # parameters don't matter, just need beams and brho defined
-    fake_knob_values = {knob: random.random() for knob in ALL_ARC_CORRECTOR_KNOBS}
-    with madx.batch():
-        madx.globals.update(fake_knob_values)
-    arc_knobs = query_arc_correctors_powering(madx)
-
-    assert all(knob in arc_knobs for knob in ALL_ARC_CORRECTOR_KNOBS)
-    assert all(knob_value != 0 for knob_value in arc_knobs.values())
+        assert all(knob in arc_knobs for knob in ALL_ARC_CORRECTOR_KNOBS)
+        assert all(knob_value != 0 for knob_value in arc_knobs.values())
