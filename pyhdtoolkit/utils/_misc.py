af1f9799a803b81566dae12b58e3d1313d53ea33
Felix Soubelet <felix.soubelet@protonmail.com>
2022-02-08 14:36:31 +0100

    add docstrings and additionnal checks for betastar fetching

--- pyhdtoolkit/utils/_misc.py
+++ pyhdtoolkit/utils/_misc.py
@@ -4,6 +4,7 @@ Module utils.misc
 
 Private module that provides miscellaneous personnal utility functions.
 """
+import shlex
 
 from multiprocessing import cpu_count
 from pathlib import Path
@@ -37,7 +38,17 @@ def fullpath(filepath: Path) -> str:
 
 
 def get_opticsfiles_paths() -> List[Path]:
-    """Call optics from the appropriate location depending on where the program runs, either 'afs' or 'local'."""
+    """
+    Returns Path objects to the opticsfiles from the appropriate location depending on where the program runs,
+    either 'afs' or 'local'. Only the normal configuration opticsfiles are returned, so those ending with a
+    special suffix such as `_ctpps1` are ignored.
+
+    Raises:
+        ValueError: If the program is running in an unknown location (neither 'afs' nor 'local').
+
+    Returns:
+        List[Path]: List of Path objects to the opticsfiles.
+    """
     if RUN_LOCATION not in ("afs", "local"):
         logger.error("Unknown runtime location, exiting")
         raise ValueError("The 'RUN_LOCATION' variable should be either 'afs' or 'local'.")
@@ -52,12 +63,24 @@ def get_opticsfiles_paths() -> List[Path]:
 
 
 def get_betastar_from_opticsfile(opticsfile: Path) -> float:
-    """Parses the betastar value from the opticsfile content, which is in the first lines."""
+    """
+    Parses the betastar value from the opticsfile content, which is in the first lines. This contains a check
+    that ensures the betastar is the same for IP1 and IP5.
+
+    Args:
+        opticsfile (Path): Path object to the opticsfile.
+
+    Raises:
+        AssertionError: if the betastar value for IP1 and IP5 is not the same (in both planes too).
+
+    Returns:
+        The betastar value parsed from the file.
+    """
     file_lines = opticsfile.read_text().split("\n")
-    ip1_line, ip5_line = [
-        line for line in file_lines if line.startswith("betx_")
-    ]  # these are for betxstar, symmetry makes it ok
-    betastar_ip1 = float(shlex.split(ip1_line)[2])
-    betastar_ip5 = float(shlex.split(ip5_line)[2])
-    assert betastar_ip1 == betastar_ip5
-    return betastar_ip1
+    ip1_x_line, ip1_y_line, ip5_x_line, ip5_y_line = [line for line in file_lines if line.startswith("bet")]
+    betastar_x_ip1 = float(shlex.split(ip1_x_line)[2])
+    betastar_y_ip1 = float(shlex.split(ip1_y_line)[2])
+    betastar_x_ip5 = float(shlex.split(ip5_x_line)[2])
+    betastar_y_ip5 = float(shlex.split(ip5_y_line)[2])
+    assert betastar_x_ip1 == betastar_y_ip1 == betastar_x_ip5 == betastar_y_ip5
+    return betastar_x_ip1  # doesn't matter which plane, they're all the same
