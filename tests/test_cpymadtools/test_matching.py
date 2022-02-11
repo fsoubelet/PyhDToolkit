5eecee7050d34f43d35897b0a9281662a38a17d1
Felix Soubelet <felix.soubelet@protonmail.com>
2022-02-11 14:14:21 +0100

    move cta test to created test_coupling

--- tests/test_cpymadtools/test_matching.py
+++ tests/test_cpymadtools/test_matching.py
@@ -7,7 +7,6 @@ from cpymad.madx import Madx
 from pyhdtoolkit.cpymadtools.generators import LatticeGenerator
 from pyhdtoolkit.cpymadtools.lhc import apply_lhc_coupling_knob
 from pyhdtoolkit.cpymadtools.matching import (
-    get_closest_tune_approach,
     get_lhc_tune_and_chroma_knobs,
     match_tunes_and_chromaticities,
 )
@@ -113,39 +112,3 @@ class TestMatching:
         )
         assert math.isclose(madx.table.summ.dq1[0], dq1_target, rel_tol=1e-3)
         assert math.isclose(madx.table.summ.dq2[0], dq2_target, rel_tol=1e-3)
-
-    @pytest.mark.parametrize("telescopic_squeeze", [False, True])
-    def test_closest_tune_approach(self, _non_matched_lhc_madx, telescopic_squeeze):
-        """Using LHC lattice."""
-        madx = _non_matched_lhc_madx
-        apply_lhc_coupling_knob(madx, 2e-3, telescopic_squeeze=telescopic_squeeze)
-        match_tunes_and_chromaticities(
-            madx, "lhc", "lhcb1", 62.31, 60.32, 2.0, 2.0, telescopic_squeeze=telescopic_squeeze
-        )
-
-        knobs = get_lhc_tune_and_chroma_knobs("lhc", telescopic_squeeze=telescopic_squeeze)
-        knobs_before = {knob: madx.globals[knob] for knob in knobs}
-        cminus = get_closest_tune_approach(madx, "lhc", "lhcb1", telescopic_squeeze=telescopic_squeeze)
-        knobs_after = {knob: madx.globals[knob] for knob in knobs}  # should be put back
-
-        assert math.isclose(cminus, 2e-3, rel_tol=1e-1)  # let's say 10% as MAD-X does what it can
-        assert knobs_after == knobs_before
-
-    @pytest.mark.parametrize("telescopic_squeeze", [False, True])
-    def test_closest_tune_approach_with_explicit_targets(self, _non_matched_lhc_madx, telescopic_squeeze):
-        """Using LHC lattice."""
-        madx = _non_matched_lhc_madx
-        apply_lhc_coupling_knob(madx, 2e-3, telescopic_squeeze=telescopic_squeeze)
-        match_tunes_and_chromaticities(
-            madx, "lhc", "lhcb1", 62.31, 60.32, 2.0, 2.0, telescopic_squeeze=telescopic_squeeze
-        )
-
-        knobs = get_lhc_tune_and_chroma_knobs("lhc", telescopic_squeeze=telescopic_squeeze)
-        knobs_before = {knob: madx.globals[knob] for knob in knobs}
-        cminus = get_closest_tune_approach(
-            madx, "lhc", "lhcb1", explicit_targets=(62.315, 60.315), telescopic_squeeze=telescopic_squeeze
-        )
-        knobs_after = {knob: madx.globals[knob] for knob in knobs}  # should be put back
-
-        assert math.isclose(cminus, 2e-3, rel_tol=1e-1)  # let's say 10% as MAD-X does what it can
-        assert knobs_after == knobs_before
