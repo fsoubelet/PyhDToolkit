19a6c7db062203cbdb2f25c75202e79c76745df3
Felix Soubelet <felix.soubelet@protonmail.com>
2022-02-11 14:16:22 +0100

    move lhc knobs tests to test_lhc

--- tests/test_cpymadtools/test_matching.py
+++ tests/test_cpymadtools/test_matching.py
@@ -6,46 +6,12 @@ from cpymad.madx import Madx
 
 from pyhdtoolkit.cpymadtools.generators import LatticeGenerator
 from pyhdtoolkit.cpymadtools.lhc import apply_lhc_coupling_knob
-from pyhdtoolkit.cpymadtools.matching import (
-    get_lhc_tune_and_chroma_knobs,
-    match_tunes_and_chromaticities,
-)
+from pyhdtoolkit.cpymadtools.matching import match_tunes_and_chromaticities
 
 BASE_LATTICE = LatticeGenerator.generate_base_cas_lattice()
 
 
 class TestMatching:
-    @pytest.mark.parametrize("beam", [1, 2, 3, 4])
-    @pytest.mark.parametrize("telescopic_squeeze", [False, True])
-    def test_lhc_tune_and_chroma_knobs(self, beam, telescopic_squeeze):
-        expected_beam = 2 if beam == 4 else beam
-        expected_suffix = "_sq" if telescopic_squeeze else ""
-        assert get_lhc_tune_and_chroma_knobs("LHC", beam, telescopic_squeeze) == (
-            f"dQx.b{expected_beam}{expected_suffix}",
-            f"dQy.b{expected_beam}{expected_suffix}",
-            f"dQpx.b{expected_beam}{expected_suffix}",
-            f"dQpy.b{expected_beam}{expected_suffix}",
-        )
-
-    @pytest.mark.parametrize("beam", [1, 2, 3, 4])
-    @pytest.mark.parametrize("telescopic_squeeze", [False, True])
-    def test_hllhc_tune_and_chroma_knobs(self, beam, telescopic_squeeze):
-        expected_beam = 2 if beam == 4 else beam
-        expected_suffix = "_sq" if telescopic_squeeze else ""
-        assert get_lhc_tune_and_chroma_knobs("HLLHC", beam, telescopic_squeeze) == (
-            f"kqtf.b{expected_beam}{expected_suffix}",
-            f"kqtd.b{expected_beam}{expected_suffix}",
-            f"ksf.b{expected_beam}{expected_suffix}",
-            f"ksd.b{expected_beam}{expected_suffix}",
-        )
-
-    def test_get_knobs_fails_on_unknown_accelerator(self, caplog):
-        with pytest.raises(NotImplementedError):
-            _ = get_lhc_tune_and_chroma_knobs("not_an_accelerator")
-
-        for record in caplog.records:
-            assert record.levelname == "ERROR"
-
     @pytest.mark.parametrize("q1_target, q2_target", [(6.335, 6.29), (6.34, 6.27), (6.38, 6.27)])
     @pytest.mark.parametrize("dq1_target, dq2_target", [(100, 100), (95, 95), (105, 105)])
     @pytest.mark.parametrize("telescopic_squeeze", [False, True])
