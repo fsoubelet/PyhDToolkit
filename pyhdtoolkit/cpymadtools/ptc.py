10ec878ab31da84cb71e483c3ed06d5e0e350b08
Felix Soubelet <felix.soubelet@protonmail.com>
2022-02-20 15:22:09 +0100

    doc overhaul

--- pyhdtoolkit/cpymadtools/ptc.py
+++ pyhdtoolkit/cpymadtools/ptc.py
@@ -4,7 +4,8 @@
 PTC Routines
 ------------
 
-A module with functions to harness MAD-X PTC functionality with a cpymad.madx.Madx object.
+Module with functions to manipulate ``MAD-X`` ``PTC`` functionality through a
+`~cpymad.madx.Madx` object.
 """
 from pathlib import Path
 from typing import Dict, Sequence, Tuple, Union
@@ -22,33 +23,38 @@ def get_amplitude_detuning(
     madx: Madx, order: int = 2, file: Union[Path, str] = None, fringe: bool = False, **kwargs
 ) -> tfs.TfsDataFrame:
     """
-    INITIAL IMPLEMENTATION CREDITS GO TO JOSCHUA DILLY (@JoschD), but this has been heavily refactored.
-    Calculate amplitude detuning via `PTC_NORMAL`, with sensible defaults set for other relevant `PTC`
-    commands. The result table is returned as a `TfsDataFrame`, the headers of which are the contents of the
-    internal `SUMM` table.
+    Calculates amplitude detuning coefficients via ``PTC_NORMAL``, with sensible defaults set for
+    other relevant ``PTC`` commands used in the process. The result table is returned as a
+    `~tfs.frame.TfsDataFrame`, the headers of which are the contents of the internal ``SUMM`` table.
+    This is a heavily refactored version of an initial implementation by :user:`Joschua Dilly <joschd>`.
 
-    The `PTC_CREATE_LAYOUT` command is issued with `model=3` (`SixTrack` model), `method=4` (integration
-    order), `nst=3` (number of integration steps, aka body slices for elements) and `exact=True` (use
-    exact Hamiltonian, not an approximated one).
+    .. important::
+        The ``PTC_CREATE_LAYOUT`` command is issued with ``model=3`` (``SixTrack`` model), ``method=4``
+        (integration order), ``nst=3`` (number of integration steps, aka body slices for elements) and
+        ``exact=True`` (use exact Hamiltonian, not an approximated one).
 
-    The `PTC_NORMAL` command is explicitely given `icase=6` to enforce 6D calculations (see the `MAD-X`
-    manual for details), `no=5` (map order for derivative evaluation of Twiss parameters) and
-    `closedorbit=True` (triggers closed orbit calculation) and `normal=True` (activate calculation of the
-    Normal Form).
+        The ``PTC_NORMAL`` command is explicitely given ``icase=6`` to enforce 6D calculations (see the
+        `MAD-X manual <http://madx.web.cern.ch/madx/releases/last-rel/madxuguide.pdf>`_ for details),
+        ``no=5`` (map order for derivative evaluation of Twiss parameters), ``closedorbit=True`` (triggers
+        closed orbit calculation) and ``normal=True`` (activate calculation of the Normal Form).
 
     Args:
         madx (cpymad.madx.Madx): an instanciated `~cpymad.madx.Madx` object.
-        order (int): maximum derivative order coefficient (only 0, 1 or 2 implemented in `PTC`).
-            Defaults to `2`.
-        file (Union[Path, str]): path to output file. Default `None`
-        fringe (bool): boolean flag to include fringe field effects in the calculation. Defaults to `False`.
-
-    Keyword Args:
-        Any keyword argument is transmitted to the `PTC_NORMAL` command. See above which arguments are
-        already set for `PTC_NORMAL` to avoid trying to override them.
+        order (int): maximum derivative order coefficient (only 0, 1 or 2 implemented in ``PTC``).
+            Defaults to 2.
+        file (Union[Path, str]): path to output file. Defaults to `None`.
+        fringe (bool): boolean flag to include fringe field effects in the calculation. Defaults to
+            ``False``.
+        **kwargs: any keyword argument is transmitted to the ``PTC_NORMAL`` command. See above which
+            arguments are already set for ``PTC_NORMAL`` to avoid trying to override them.
 
     Returns:
-        A `TfsDataframe` with results.
+        A `~tfs.frame.TfsDataframe` with the calculated coefficients.
+
+    Example:
+        .. code-block:: python
+
+            >>> ampdet_coeffs = get_amplitude_detuning(madx, order=2, closedorbit=True)
     """
     if order >= 3:
         logger.error(f"Maximum amplitude detuning order in PTC is 2, but {order:d} was requested")
@@ -108,32 +114,37 @@ def get_rdts(
     madx: Madx, order: int = 4, file: Union[Path, str] = None, fringe: bool = False, **kwargs
 ) -> tfs.TfsDataFrame:
     """
-    INITIAL IMPLEMENTATION CREDITS GO TO JOSCHUA DILLY (@JoschD), but this has been heavily refactored.
-    Calculate the `RDTs` via `PTC_TWISS`, with sensible defaults set for other relevant `PTC` commands.
-    The result table is returned as a `TfsDataFrame`, the headers of which are the contents of the
-    internal `SUMM` table.
+    Calculate the resonance driving terms up to *order* via ``PTC_TWISS``, with sensible defaults
+    set for other relevant ``PTC`` commands. The result table is returned as a `~tfs.frame.TfsDataFrame`,
+    the headers of which are the contents of the internal ``SUMM`` table. This is a heavily refactored
+    version of an initial implementation by :user:`Joschua Dilly <joschd>`.
 
-    The `PTC_CREATE_LAYOUT` command is issued with `model=3` (`SixTrack` model), `method=4` (integration
-    order), `nst=3` (number of integration steps, aka body slices for elements) and `exact=True` (use
-    exact Hamiltonian, not an approximated one).
+    .. important::
+        The ``PTC_CREATE_LAYOUT`` command is issued with ``model=3`` (``SixTrack`` model), ``method=4``
+        (integration order), ``nst=3`` (number of integration steps, aka body slices for elements) and
+        ``exact=True`` (use exact Hamiltonian, not an approximated one).
 
-    The `PTC_TWISS` command is explicitely given `icase=6` to enforce 6D calculations (see the `MAD-X`
-    manual for details), `trackrdts=True` (for this function to fullfill its purpose) and `normal=True` to
-    trigger saving the normal form analysis results in a table called `NONLIN` which will then be available
-    through the provided `Madx` instance.
+        The ``PTC_NORMAL`` command is explicitely given ``icase=6`` to enforce 6D calculations (see the
+        `MAD-X manual <http://madx.web.cern.ch/madx/releases/last-rel/madxuguide.pdf>`_ for details),
+        ``trackrdts=True`` (for this function to fullfill its purpose) and ``normal=True`` to trigger
+        saving the normal form analysis results in a table called ``NONLIN`` which will then be available
+        through the provided `~cpymad.madx.Madx` instance.
 
     Args:
         madx (cpymad.madx.Madx): an instanciated `~cpymad.madx.Madx` object.
-        order (int): map order for derivative evaluation of Twiss parameters. Defaults to `4`.
-        file (Union[Path, str]): path to output file. Default `None`
+        order (int): map order for derivative evaluation of Twiss parameters. Defaults to 4.
+        file (Union[Path, str]): path to output file. Default to `None`.
         fringe (bool): boolean flag to include fringe field effects in the calculation. Defaults to `False`.
-
-    Keyword Args:
-        Any keyword argument is transmitted to the `PTC_TWISS` command. See above which arguments are
-        already set for `PTC_TWISS` to avoid trying to override them.
+        **kwargs: any keyword argument is transmitted to the ``PTC_TWISS`` command. See above which arguments
+            are already set for ``PTC_TWISS`` to avoid trying to override them.
 
     Returns:
-        A `TfsDataframe` with results.
+        A `TfsDataframe` with the calculated RDTs.
+
+    Example:
+        .. code-block:: python
+
+            >>> rdts_df = get_rdts(madx, order=3, fringe=True)
     """
     logger.info("Creating PTC universe")
     madx.ptc_create_universe()
@@ -162,34 +173,40 @@ def ptc_twiss(
     madx: Madx, order: int = 4, file: Union[Path, str] = None, fringe: bool = False, table: str = "ptc_twiss", **kwargs
 ) -> tfs.TfsDataFrame:
     """
-    Calculate the TWISS parameters according to G. Ripken's formalism via `PTC_TWISS`, with sensible defaults
-    set for other relevant `PTC` commands. The result table is returned as a `TfsDataFrame`, the headers of
-    which are the contents of the internal `SUMM` table.
+    Calculates the ``TWISS`` parameters according to the :cite:t:`Ripken:optics:1989` formalism
+    via ``PTC_TWISS``, with sensible defaults set for other relevant ``PTC`` commands. The result
+    table is returned as a `~tfs.frame.TfsDataFrame`, the headers of which are the contents of the
+    internal ``SUMM`` table.
 
-    This is very similar to the `get_rdts` function as both use `ptc_twiss` internally, however this function
-    does not track RDTs which makes the calculations significantly faster.
+    This is very similar to the `~.ptc.get_rdts` function as both use ``PTC_TWISS`` internally,
+    however this function does not track RDTs which makes the calculations significantly faster.
 
-    The `PTC_CREATE_LAYOUT` command is issued with `model=3` (`SixTrack` model), `method=4` (integration
-    order), `nst=3` (number of integration steps, aka body slices for elements) and `exact=True` (use
-    exact Hamiltonian, not an approximated one).
+    .. important::
+        The ``PTC_CREATE_LAYOUT`` command is issued with ``model=3`` (``SixTrack`` model), ``method=4``
+        (integration order), ``nst=3`` (number of integration steps, aka body slices for elements) and
+        ``exact=True`` (use exact Hamiltonian, not an approximated one).
 
-    The `PTC_TWISS` command is explicitely given `icase=6` to enforce 6D calculations (see the `MAD-X`
-    manual for details) `normal=True` to trigger saving the normal form analysis results in a table called
-    `NONLIN` which will then be available through the provided `Madx` instance.
+        The ``PTC_TWISS`` command is explicitely given ``icase=6`` to enforce 6D calculations (see the
+        `MAD-X manual <http://madx.web.cern.ch/madx/releases/last-rel/madxuguide.pdf>`_ for details),
+        ``normal=True`` to trigger saving the normal form analysis results in a table called ``NONLIN``
+        which will then be available through the provided `~cpymad.madx.Madx` instance.
 
     Args:
         madx (cpymad.madx.Madx): an instanciated `~cpymad.madx.Madx` object.
-        order (int): map order for derivative evaluation of Twiss parameters. Defaults to `4`.
-        file (Union[Path, str]): path to output file. Default `None`
+        order (int): map order for derivative evaluation of ``TWISS`` parameters. Defaults to 4.
+        file (Union[Path, str]): path to output file. Default to `None`.
         fringe (bool): boolean flag to include fringe field effects in the calculation. Defaults to `False`.
-        table (str): the name of the internal table in which to save the results. Defaults to 'ptc_twiss'.
-
-    Keyword Args:
-        Any keyword argument is transmitted to the `PTC_TWISS` command. See above which arguments are
-        already set for `PTC_TWISS` to avoid trying to override them.
+        table (str): the name of the internal table in which to save the results. Defaults to **ptc_twiss**.
+        **kwargs: Any keyword argument is transmitted to the ``PTC_TWISS`` command. See above which arguments
+            are already set for `PTC_TWISS` to avoid trying to override them.
 
     Returns:
-        A `TfsDataframe` with results.
+        A `TfsDataframe` with the calculated ``TWISS`` parameters.
+
+    Example:
+        .. code-block:: python
+
+            >>> twiss_ptc_df = ptc_twiss(madx, order=3)
     """
     logger.info(f"Creating PTC universe")
     madx.ptc_create_universe()
@@ -225,48 +242,50 @@ def ptc_track_particle(
     **kwargs,
 ) -> Dict[str, pd.DataFrame]:
     """
-    Tracks a single particle for nturns through PTC_TRACK, based on its initial coordinates.
+    Tracks a single particle for *nturns* through ``PTC_TRACK``, based on its initial coordinates. The
+    use of this function is similar to that of `~.track.track_single_particle`.
 
-    The `PTC_CREATE_LAYOUT` command is issued with `model=3` (`SixTrack` model), `method=4` (integration
-    order), `nst=3` (number of integration steps, aka body slices for elements) and `exact=True` (use
-    exact Hamiltonian, not an approximated one).
+    .. important::
+        The ``PTC_CREATE_LAYOUT`` command is issued with ``model=3`` (``SixTrack`` model), ``method=4``
+        (integration order), ``nst=3`` (number of integration steps, aka body slices for elements) and
+        ``exact=True`` (use exact Hamiltonian, not an approximated one).
 
-    The `PTC_TRACK` command is explicitely given `ELEMENT_BY_ELEMENT=True` to force element by element
-    tracking mode.
+        The ``PTC_TRACK`` command is explicitely given ``ELEMENT_BY_ELEMENT=True`` to force element by
+        element tracking mode.
 
-    Warnings:
-        If the `sequence` argument is given a string value, the `USE` command will be ran on the provided
-        sequence name. This means the caveats of `USE` apply, for instance the erasing of previously
+    .. warning::
+        If the *sequence* argument is given a string value, the ``USE`` command will be ran on the provided
+        sequence name. This means the caveats of ``USE`` apply, for instance the erasing of previously
         defined errors, orbits corrections etc. In this case a warning will be logged but the function will
-        proceed. If `None` is given (by default) then the sequence already in used will be the one tracking
+        proceed. If `None` is given (by default) then the sequence already in use will be the one tracking
         is performed on.
 
     Args:
         madx (Madx): an instantiated cpymad.madx.Madx object.
-        initial_coordinates (Tuple[float, float, float, float, float, float]): a tuple with the X, PX, Y, PY,
-            T, PT starting coordinates the particle to track. Defaults to all 0 if none given.
+        initial_coordinates (Tuple[float, float, float, float, float, float]): a tuple with the ``X, PX,
+            Y, PY, T, PT`` starting coordinates of the particle to track. Defaults to all 0 if `None` given.
         nturns (int): the number of turns to track for.
-        sequence (str): the sequence to use for tracking. If no value is provided, it is assumed that a
-            sequence is already defined and in use, and this one will be picked up by MAD-X. BEWARE of the
-            dangers of giving a sequence that will be `use`d by `MAD-X`, see function warning docstring.
-        observation_points (Sequence[str]): sequence of all element names at which to OBSERVE during the
+        sequence (Optional[str]): the sequence to use for tracking. If no value is provided, it is assumed
+            that a sequence is already defined and in use, and this one will be picked up by ``MAD-X``.
+            Beware of the dangers of giving a sequence that will be used by ``MAD-X``, see the warning below
+            for more information.
+        observation_points (Sequence[str]): sequence of all element names at which to ``OBSERVE`` during the
             tracking.
         onetable (bool): flag to combine all observation points data into a single table. Defaults to `False`.
         fringe (bool): boolean flag to include fringe field effects in the calculation. Defaults to `False`.
-
-    Keyword Args:
-        Any keyword argument to be given to the `PTC_TRACK` command, such as the `CLOSED_ORBIT` flag to
-        activate closed orbit calculation before tracking. Refer to the `MAD-X` manual for options.
+        **kwargs: Any keyword argument is transmitted to the ``PTC_TRACK`` command such as the ``CLOSED_ORBIT``
+            flag to activate closed orbit calculation before tracking. Refer to the
+            `MAD-X manual <http://madx.web.cern.ch/madx/releases/last-rel/madxuguide.pdf>`_ for options.
 
     Returns:
-        A dictionary with a copy of the track table's dataframe for each defined observation point,
-        with as columns the coordinates x, px, y, py, t, pt, s and e (energy). The keys of the dictionary
-        are simply numbered 'observation_point_1', 'observation_point_2' etc. The first observation point
+        A `dict` with a copy of the track table's dataframe for each defined observation point,
+        with as columns the coordinates ``x, px, y, py, t, pt, s and e`` (energy). The keys of the dictionary
+        are simply named ``observation_point_1``, ``observation_point_2`` etc. The first observation point
         always corresponds to the start of machine, the others correspond to the ones manually defined,
         in the order they are defined in.
 
-        If the user has set `onetable` to `True`, only one entry is in the dictionary under the key
-        'trackone' and it has the combined table as a pandas DataFrame for value.
+        If the user has set ``onetable`` to `True`, only one entry is in the dictionary under the key
+        ``trackone`` and it has the combined table as a pandas DataFrame for value.
     """
     logger.info("Performing single particle PTC (thick) tracking")
     start = initial_coordinates if initial_coordinates else [0, 0, 0, 0, 0, 0]
