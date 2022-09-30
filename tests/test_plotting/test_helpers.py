8423f1bd8c2f2936cdd1a8aceee286a9d8664a2c
Felix Soubelet <felix.soubelet@protonmail.com>
2022-09-30 15:47:12 +0100

    no class

--- tests/test_plotting/test_helpers.py
+++ tests/test_plotting/test_helpers.py
@@ -8,20 +8,19 @@ from pyhdtoolkit.plotting.helpers import set_arrow_label
 matplotlib.use("Agg")
 
 
-class TestAnnotationsPlotter:
-    @pytest.mark.mpl_image_compare(tolerance=20, style="seaborn-pastel", savefig_kwargs={"dpi": 200})
-    def test_set_arrow_label(self):
-        figure = plt.figure(figsize=(12, 7))
-        ax = figure.add_subplot()
-        ax.scatter(0, 0)
-        ax.set_xlim((-1, 2))
-        ax.set_ylim((-1, 2))
-        set_arrow_label(
-            axis=ax,
-            label="Test label on a test point",
-            color="b",
-            arrow_position=(0, 0),
-            label_position=(1, 1),
-            arrow_arc_rad=-0.2,
-        )
-        return figure
+@pytest.mark.mpl_image_compare(tolerance=20, savefig_kwargs={"dpi": 200})
+def test_set_arrow_label():
+    figure = plt.figure(figsize=(12, 7))
+    ax = figure.add_subplot()
+    ax.scatter(0, 0)
+    ax.set_xlim((-1, 2))
+    ax.set_ylim((-1, 2))
+    set_arrow_label(
+        axis=ax,
+        label="Test label on a test point",
+        color="b",
+        arrow_position=(0, 0),
+        label_position=(1, 1),
+        arrow_arc_rad=-0.2,
+    )
+    return figure
