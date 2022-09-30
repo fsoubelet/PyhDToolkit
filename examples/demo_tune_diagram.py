e0a37b9aa0c7f7bce31ca0a39dc93732f4e95ed3
Felix Soubelet <felix.soubelet@protonmail.com>
2022-09-30 12:28:18 +0100

    adapt tune diagram plotting example

--- examples/demo_tune_diagram.py
+++ examples/demo_tune_diagram.py
@@ -12,7 +12,7 @@ to visualise resonance lines up to certain orders.
 # sphinx_gallery_thumbnail_number = -1
 import matplotlib.pyplot as plt
 
-from pyhdtoolkit.cpymadtools.plotters import TuneDiagramPlotter
+from pyhdtoolkit.cpymadtools.plot.tune import plot_tune_diagram
 from pyhdtoolkit.utils import defaults
 
 defaults.config_logger(level="warning")
@@ -26,7 +26,8 @@ plt.rcParams.update(defaults._SPHINX_GALLERY_PARAMS)  # for readability of this 
 # decreasing with the resonance order. The max order is set 6, as the graph becomes unreadable
 # above this value.
 
-TuneDiagramPlotter.plot_tune_diagram(max_order=5)
+fig, ax = plt.subplots(figsize=(8, 8))
+plot_tune_diagram(max_order=5)
 plt.show()
 
 ###############################################################################
@@ -39,7 +40,8 @@ plt.show()
 #     when determining the stability. As so, plot that value onto the graph, or it would
 #     be way out of range
 
-TuneDiagramPlotter.plot_tune_diagram(max_order=5)
+fig, ax = plt.subplots(figsize=(8, 8))
+plot_tune_diagram(max_order=5)
 plt.scatter(0.35, 0.29, marker="o", color="red")  # this is close to a third order resonance
 plt.show()
 
@@ -49,12 +51,13 @@ plt.show()
 # line, aka knowing which resonance order is responsible for each given line. This
 # makes it easier to see the resonances near your working point.
 
-TuneDiagramPlotter.plot_tune_diagram(
+fig, ax = plt.subplots(figsize=(8, 8))
+plot_tune_diagram(
     max_order=6,
     differentiate_orders=True,  # different orders will have a different color and linestyle
     legend_title="Resonance Lines",  # if given, legend is added to figure
 )
-plt.scatter(0.35, 0.29, marker="o", color="m")  # this is close to a third order resonance
+plt.scatter(0.35, 0.29, marker="o", color="red")  # this is close to a third order resonance
 plt.xlim(0, 0.5)  # limit the x-axis to the range [0, 0.5] for visibility
 plt.ylim(0, 0.5)  # limit the y-axis to the range [0, 0.5] for visibility
 plt.show()
@@ -69,4 +72,4 @@ plt.show()
 #    The use of the following functions, methods, classes and modules is shown
 #    in this example:
 #
-#    - `~.cpymadtools.plotters.TuneDiagramPlotter`: `~.plotters.TuneDiagramPlotter.plot_tune_diagram`
+#    - `.cpymadtools.plot.tune`: `~.cpymadtools.plot.tune.plot_tune_diagram`
