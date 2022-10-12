1eba34b59f54e0ea167381d32784bd4e988cdd6f
Felix Soubelet <felix.soubelet@protonmail.com>
2022-10-12 12:01:08 +0100

    thesis small done

--- pyhdtoolkit/plotting/styles/thesis.py
+++ pyhdtoolkit/plotting/styles/thesis.py
@@ -20,7 +20,47 @@ from typing import Dict, Union
 PlotSetting = Union[float, bool, str, tuple]
 
 
-SMALL: Dict[str, PlotSetting] = {}
+SMALL: Dict[str, PlotSetting] = {
+    # ------ Patches ------ #
+    "patch.linewidth": 1.3,  # Width of patches edge lines
+    # ------ Fonts ------ #
+    "font.size": 11,  # Default font size of elements
+    # ----- Mathtext ----- #
+    "mathtext.default": "regular",  # Default font for math
+    # ------ Text ------ #
+    "text.usetex": False,  # Don't use LaTeX for text handling
+    # ------ Axes ------ #
+    "axes.linewidth": 1,  # Linewidth of axes edges
+    "axes.titlesize": 15,  # Fontsize of the axes title
+    "axes.labelsize": 15,  # Fontsize of the x and y axis labels
+    "axes.formatter.limits": (-4, 5),  # Switch to scientific notations when order of magnitude reaches ±1e3
+    "axes.formatter.use_mathtext": True,  # Format with i.e 10^{4} instead of 1e4
+    "axes.formatter.useoffset": False,  # Do not use the annoying offset on top of yticks
+    # ------ Horizontal Ticks ------ #
+    "xtick.major.size": 4,  # Size (length) of the major xtick locators
+    "xtick.major.width": 1,  # Width of the major xtick locators
+    "xtick.labelsize": 12,  # Fontsize of the x axis tick labels
+    "xtick.direction": "in",  # Show xticks towards inside of figure
+    # ------ Vertical Ticks ------ #
+    "ytick.major.size": 4,  # Size (length) of the major ytick locators
+    "ytick.major.width": 1,  # Width of the major ytick locators
+    "ytick.labelsize": 12,  # Fontsize of the y axis tick labels
+    "ytick.direction": "in",  # Show yticks towards inside of figure
+    # ------- Legend ------ #
+    "legend.loc": "best",  # Default legend location
+    "legend.frameon": True,  # Make a dedicated patch for the legend
+    "legend.framealpha": 0.85,  # Legend patch transparency factor
+    "legend.fancybox": True,  # Use rounded box for legend background
+    "legend.fontsize": 12,  # Legend text font size
+    # ------ Figure ------ #
+    "figure.figsize": (5, 3),  # Size of the figure
+    "figure.titlesize": 15,  # Size of the figure title
+    "figure.subplot.left": 0.15,  # Left side of the subplots of the figure
+    "figure.subplot.bottom": 0.17,  # Bottom side of the subplots of the figure
+    # ------ Saving ------ #
+    "savefig.dpi": 300,  # Saved figure dots per inch
+    "savefig.format": "pdf",  # Saved figure file format
+}
 
 # TODO: adapt from these sphinx params
 MEDIUM: Dict[str, PlotSetting] = {
