6a49534816d1fa0e0fd678301d2aa587a50a30d9
Felix Soubelet <felix.soubelet@protonmail.com>
2022-10-12 15:27:38 +0100

    test install of styles

---
+++ tests/test_plotting/test_styles.py
@@ -0,0 +1,15 @@
+import pathlib
+
+import matplotlib
+import matplotlib.pyplot as plt
+import pytest
+
+from pyhdtoolkit.plotting.styles import install_mpl_styles
+
+
+def test_mplstyles_install():
+    install_mpl_styles()
+
+    for expected_name in ("thesis-small", "thesis-medium", "thesis-large", "paper-single", "paper-double"):
+        assert (pathlib.Path(matplotlib.get_configdir()) / "stylelib" / f"{expected_name}.mplstyle").is_file()
+        assert (pathlib.Path(plt.style.core.BASE_LIBRARY_PATH) / f"{expected_name}.mplstyle").is_file()
