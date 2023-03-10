44f80e0a84d394e15f47587ba88cb2f4860ec650
Felix Soubelet <felix.soubelet@protonmail.com>
2023-03-10 12:36:37 +0000

    shorten width of doc

--- pyhdtoolkit/plotting/layout.py
+++ pyhdtoolkit/plotting/layout.py
@@ -43,69 +43,89 @@ def plot_machine_layout(
     """
     .. versionadded:: 1.0.0
 
-    Draws patches elements representing the lattice layout on the given *axis*. This is
-    the function that takes care of the machine layout axis in `~.plotting.lattice.plot_latwiss`
-    and `~.plotting.aperture.plot_aperture`. Its results can be seen in the
-    :ref:`machine lattice <demo-accelerator-lattice>` and :ref:`machine aperture <demo-accelerator-aperture>`
-    example galleries.
+    Draws patches elements representing the lattice layout on the given
+    *axis*. This is the function that takes care of the machine layout axis
+    in `~.plotting.lattice.plot_latwiss` and
+    `~.plotting.aperture.plot_aperture`. Its results can be seen in the
+    :ref:`machine lattice <demo-accelerator-lattice>` and
+    :ref:`machine aperture <demo-accelerator-aperture>` example galleries.
 
     .. note::
-        This current implementation can plot dipoles, quadrupoles, sextupoles and BPMs.
+        This current implementation can plot dipoles, quadrupoles, sextupoles,
+        octupoles and BPMs.
 
     .. important::
-        If not provided, the limits for the ``k0l_lim``, ``k1l_lim`` will be auto-determined, which might
-        not be the perfect choice for you plot. When providing these limits (also for ``k2l_lim``), make
-        sure to provide symmetric values around 0 (so [-x, x]) otherwise the element patches will show up
-        vertically displaced from the axis' center line.
+        If not provided, the limits for the ``k0l_lim``, ``k1l_lim`` will be
+        auto-determined, which might not be the perfect choice for you plot.
+        When providing these limits (also for ``k2l_lim``), make sure to
+        provide symmetric values around 0 (so [-x, x]) otherwise the element
+        patches will show up vertically displaced from the axis' center line.
 
     .. warning::
-        Currently the function tries to plot legends for the different layout patches. The position of the
-        different legends has been hardcoded in corners of the `~matplotlib.axes.Axes` and might require users
-        to tweak the axis limits (through ``k0l_lim``, ``k1l_lim`` and ``k2l_lim``) to ensure legend labels and
-        plotted elements don't overlap.
+        Currently the function tries to plot legends for the different layout
+        patches. The position of the different legends has been hardcoded in
+        corners of the `~matplotlib.axes.Axes` and might require users to tweak
+        the axis limits (through ``k0l_lim``, ``k1l_lim`` and ``k2l_lim``) to
+        ensure legend labels and plotted elements don't overlap.
 
     Args:
-        madx (cpymad.madx.Madx): an instanciated `~cpymad.madx.Madx` object. Positional only.
-        title (Optional[str]): if provided, is set as title of the plot. Defaults to `None`.
-        xoffset (float): An offset applied to the ``S`` coordinate before plotting. This is useful if
-            you want to center a plot around a specific point or element, which would then become located
-            at :math:`s = 0`. Beware this offset is applied before applying the *xlimits*. Defaults to 0.
-        xlimits (Tuple[float, float]): will implement xlim (for the ``s`` coordinate) if this is
-            not ``None``, using the tuple passed.
-        plot_dipoles (bool): if `True`, dipole patches will be plotted on the layout subplot of
-            the figure. Defaults to `True`. Dipoles are plotted in blue.
-        plot_dipole_k1 (bool): if `True`, dipole elements with a quadrupolar gradient will have this
-            gradient plotted as a quadrupole patch. Defaults to `False`.
-        plot_quadrupoles (bool): if `True`, quadrupole patches will be plotted on the layout
-            subplot of the figure. Defaults to `True`. Quadrupoles are plotted in red.
-        plot_bpms (bool): if `True`, additional patches will be plotted on the layout subplot to
-            represent Beam Position Monitors. BPMs are plotted in dark grey.
-        k0l_lim (Union[Tuple[float, float], float, int]): vertical axis limits for the ``k0l``
-            values used for the height of dipole patches. Can be given as a single value (float,
-            int) or a tuple (in which case it should be symmetric). If `None` (default) is given,
-            then the limits will be auto-determined based on the ``k0l`` values of the dipoles in
-            the plot.
-        k1l_lim (Union[Tuple[float, float], float, int]): vertical axis limits for the ``k1l``
-            values used for the height of quadrupole patches. Can be given as a single value (float,
-            int) or a tuple (in which case it should be symmetric). If `None` (default) is given,
-            then the limits will be auto-determined based on the ``k0l`` values of the quadrupoles
-            in the plot.
-        k2l_lim (Union[Tuple[float, float], float, int]): if given, sextupole patches will be plotted
-            on the layout subplot of the figure. If given, acts as vertical axis limits for the k2l
-            values used for the height of sextupole patches. Can be given as a single value (float,
-            int) or a tuple (in which case it should be symmetric).
-        k3l_lim (Union[Tuple[float, float], float, int]): if given, octupole patches will be plotted
-            on the layout subplot of the figure. If given, acts as vertical axis limits for the k3l
-            values used for the height of octupole patches. Can be given as a single value (float,
-            int) or a tuple (in which case it should be symmetric).
-        **kwargs: any keyword argument will be transmitted to `~.plotting.utils.plot_machine_layout`, later on
-            to `~.plotting.utils._plot_lattice_series`, and then `~matplotlib.patches.Rectangle`, such as ``lw``
-            etc. If either `ax` or `axis` is found in the kwargs, the corresponding value is used as the axis
-            object to plot on. By definition, the quadrupole elements will be drawn on said axis, and for each
-            new element type to plot a call to `~matplotlib.axes.Axes.twinx` is made and the new elements will
-            be drawn on the newly created twin `~matplotlib.axes.Axes`.
-            If ``bpms_legend`` is given as False and BPMs are plotted, the BPM legend will not be plotted on
-            the layout axis.
+        madx (cpymad.madx.Madx): an instanciated `~cpymad.madx.Madx` object.
+            Positional only.
+        title (Optional[str]): if provided, is set as title of the plot.
+            Defaults to `None`.
+        xoffset (float): An offset applied to the ``S`` coordinate before
+            plotting. This is useful if you want to center a plot around a
+                specific point or element, which would then become located
+            at :math:`s = 0`. Beware this offset is applied before applying
+            the *xlimits*. Defaults to 0.
+        xlimits (Tuple[float, float]): will implement xlim (for the ``s``
+            coordinate) if this is not ``None``, using the tuple passed.
+        plot_dipoles (bool): if `True`, dipole patches will be plotted on
+            the layout subplot of the figure. Defaults to `True`. Dipoles
+            are plotted in blue.
+        plot_dipole_k1 (bool): if `True`, dipole elements with a quadrupolar
+            gradient will have this gradient plotted as a quadrupole patch.
+            Defaults to `False`.
+        plot_quadrupoles (bool): if `True`, quadrupole patches will be plotted
+            on the layout subplot of the figure. Defaults to `True`.
+            Quadrupoles are plotted in red.
+        plot_bpms (bool): if `True`, additional patches will be plotted on the
+            layout subplot to represent Beam Position Monitors. BPMs are
+            plotted in dark grey.
+        k0l_lim (Union[Tuple[float, float], float, int]): vertical axis limits
+            for the ``k0l`` values used for the height of dipole patches. Can
+            be given as a single value (float, int) or a tuple (in which case
+            it should be symmetric). If `None` (default) is given, then the
+            limits will be auto-determined based on the ``k0l`` values of the
+            dipoles in the plot.
+        k1l_lim (Union[Tuple[float, float], float, int]): vertical axis limits
+            for the ``k1l`` values used for the height of quadrupole patches.
+            Can be given as a single value (float, int) or a tuple (in which
+            case it should be symmetric). If `None` (default) is given, then
+            the limits will be auto-determined based on the ``k0l`` values of
+            the quadrupoles in the plot.
+        k2l_lim (Union[Tuple[float, float], float, int]): if given, sextupole
+            patches will be plotted on the layout subplot of the figure. If
+            given, acts as vertical axis limits for the k2l values used for
+            the height of sextupole patches. Can be given as a single value
+            (float, int) or a tuple (in which case it should be symmetric).
+        k3l_lim (Union[Tuple[float, float], float, int]): if given, octupole
+            patches will be plotted on the layout subplot of the figure. If
+            given, acts as vertical axis limits for the k3l values used for
+            the height of octupole patches. Can be given as a single value
+            (float, int) or a tuple (in which case it should be symmetric).
+        **kwargs: any keyword argument will be transmitted to
+            `~.plotting.utils.plot_machine_layout`, later on to
+            `~.plotting.utils._plot_lattice_series`, and then
+            `~matplotlib.patches.Rectangle`, such as ``lw`` etc. If either
+            `ax` or `axis` is found in the kwargs, the corresponding value
+            is used as the axis object to plot on. By definition, the
+            quadrupole elements will be drawn on said axis, and for each
+            new element type to plot a call to `~matplotlib.axes.Axes.twinx`
+            is made and the new elements will be drawn on the newly created
+            twin `~matplotlib.axes.Axes`. If ``bpms_legend`` is given as
+            `False` and BPMs are plotted, the BPM legend will not be plotted
+            on the layout axis.
 
     Example:
         .. code-block:: python
@@ -309,18 +329,21 @@ def _plot_lattice_series(
     """
     .. versionadded:: 1.0.0
 
-    Plots a `~matplotlib.patches.Rectangle` element on the provided `~matplotlib.axes.Axes` to
-    represent an element of the machine. Original code from :user:`Guido Sterbini <sterbini>`.
+    Plots a `~matplotlib.patches.Rectangle` element on the provided
+    `~matplotlib.axes.Axes` to represent an element of the machine.
+    Original code from :user:`Guido Sterbini <sterbini>`.
 
     Args:
-        ax (matplotlib.axes.Axes): an existing  `~matplotlib.axes.Axes` object to draw on.
+        ax (matplotlib.axes.Axes): an existing  `~matplotlib.axes.Axes`
+            object to draw on.
         series (pd.DataFrame): a `pandas.DataFrame` with the elements' data.
         height (float): value to reach for the patch on the y axis.
         v_offset (float): vertical offset for the patch.
         color (str): color kwarg to transmit to `~matplotlib.pyplot`.
         alpha (float): alpha kwarg to transmit to `~matplotlib.pyplot`.
-        **kwargs: any keyword argument will be transmitted to `~matplotlib.patches.Rectangle`,
-            for instance ``lw`` for the edge line width.
+        **kwargs: any keyword argument will be transmitted to
+            `~matplotlib.patches.Rectangle`, for instance ``lw`` for the
+            edge line width.
     """
     ax.add_patch(
         patches.Rectangle(
@@ -340,13 +363,15 @@ def _ylim_from_input(
     """
     .. versionadded:: 1.2.0
 
-    Determines the ylimits for a given axis from the input provided by the user.
-    This is used in `~.plotting.utils.plot_machine_layout` and handles different
-    inputs from the user, such as a tuple, a float and an int.
+    Determines the ylimits for a given axis from the input provided by the
+    user. This is used in `~.plotting.utils.plot_machine_layout` and handles
+    different inputs from the user, such as a tuple, a float and an int.
 
     Args:
-        ylim (Tuple[float, float], float, int): the input provided by the user.
-        name_for_error (str): the name of the variable to use in the error message.
+        ylim (Tuple[float, float], float, int): the input provided by
+            the user.
+        name_for_error (str): the name of the variable to use in the
+            error message.
 
     Returns:
         A tuple for the ylimits from the input.
@@ -372,12 +397,13 @@ def _determine_default_knl_lim(df: pd.DataFrame, col: str, coeff: float) -> Tupl
     """
     .. versionadded:: 1.0.0
 
-    Determine the default limits for the ``knl`` axis, when plotting machine layout.
-    This is in case `None` are provided by the user, to make sure the plot still
-    looks coherent and symmetric in `~.plotting.utils.plot_machine_layout`.
+    Determine the default limits for the ``knl`` axis, when plotting machine
+    layout. This is in case `None` are provided by the user, to make sure the
+    plot still looks coherent and symmetric in 
+    `~.plotting.utils.plot_machine_layout`.
 
-    The limits are determined symmetric, using the maximum absolute value of the
-    knl column in the provided dataframe and a 1.25 scaling factor.
+    The limits are determined symmetric, using the maximum absolute value of
+    the knl column in the provided dataframe and a 1.25 scaling factor.
 
     Args:
         df (pd.DataFrame): a `pandas.DataFrame` with the multipoles' data.
