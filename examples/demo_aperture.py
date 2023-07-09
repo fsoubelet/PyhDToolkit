403a48c1596f302d357dd3562257fa20d2abfcc4
Felix Soubelet <felix.soubelet@protonmail.com>
2023-07-09 10:22:09 +0200

    use acc-models-lhc and do some tweaks

--- examples/demo_aperture.py
+++ examples/demo_aperture.py
@@ -6,8 +6,20 @@
 Accelerator Aperture
 ====================
 
-This example shows how to use the `~.plotting.aperture.plot_aperture` function
-to visualise the available aperture in your machine, with the LHC for example.
+This example shows how to use the `~.plotting.aperture.plot_aperture` and 
+`~.plotting.aperture.plot_physical_apertures` functions to visualise the
+available aperture in your machine, with the LHC used for this example.
+
+.. important::
+    This example requires the `acc-models-lhc` repository to be cloned locally. One
+    can get it by running the following command:
+
+    .. code-block:: bash
+
+        git clone -b 2022 https://gitlab.cern.ch/acc-models/acc-models-lhc.git --depth 1
+
+    Here I set the 2022 branch for stability and reproducibility of the documentation
+    builds, but you can use any branch you want.
 """
 
 import matplotlib.pyplot as plt
@@ -23,16 +35,15 @@ logging.config_logger(level="error")
 plt.rcParams.update(_SPHINX_GALLERY_PARAMS)  # for readability of this tutorial
 
 ###############################################################################
-# Setup a simple LHC simulation in ``MAD-X``, at injection energy (450 GeV)
+# Let's start by setting up the LHC in ``MAD-X``, in this case at injection energy.
+# To understand the function below have a look at the :ref:`lhc setup example 
+# <demo-lhc-setup>`.
 
-madx = Madx(stdout=False)
-madx.option(echo=False, warn=False)
-
-madx.call("lhc/lhc_as-built.seq")
-madx.call("lhc/opticsfile.1")  # injection optics
-
-lhc.make_lhc_beams(madx, energy=450)
-madx.command.use(sequence="lhcb1")
+madx: Madx = lhc.prepare_lhc_run3(
+    opticsfile="acc-models-lhc/operation/optics/R2022a_A30cmC30cmA10mL200cm.madx",
+    energy=450,  # given in GeV
+    stdout=False
+)
 
 ###############################################################################
 # We now call the aperture definitions and tolerances, then task ``MAD-X`` with
@@ -45,6 +56,7 @@ madx.command.twiss()
 madx.command.aperture(cor=0.002, dp=8.6e-4, halo="{6,6,6,6}", bbeat=1.05, dparx=0.14, dpary=0.14)
 
 ###############################################################################
+# The details for the ``MAD-X`` ``aperture`` command can be found in the manual.
 # We can now determine the exact position of the IP5 point and plot the LHC
 # injection aperture:
 
@@ -52,7 +64,9 @@ twiss_df = madx.table.twiss.dframe()
 ip5s = twiss_df.s[twiss_df.name.str.contains("ip5")].to_numpy()[0]
 
 ###############################################################################
-# And now we can plot the aperture:
+# And now we can plot the aperture around IP5 with the `~.plotting.aperture.plot_aperture`
+# function. This uses the values in the ``APERTURE`` table of ``MAD-X``, which
+# gives aperture information in terms of beam sigma.
 
 plt.figure(figsize=(20, 13))
 plot_aperture(
@@ -80,6 +94,7 @@ plt.figure(figsize=(18, 10))
 plot_physical_apertures(madx, plane="x")
 plt.setp(plt.gca(), xlabel="S [m]", ylabel="X [m]")
 plt.ylim(-0.035, 0.035)
+plt.show()
 
 ###############################################################################
 # We can give a ``scale`` argument to change the scale of the Y-axis. Let's make
@@ -90,6 +105,7 @@ plot_physical_apertures(madx, plane="x", scale=1e2)  # just give the scaling fac
 plt.setp(plt.gca(), xlabel="S [m]", ylabel="Y [cm]")
 plt.ylim(-4, 4)
 plt.xlim(9000, 11_000)
+plt.show()
 
 ###############################################################################
 # Let's not forget to close the rpc connection to ``MAD-X``:
@@ -103,5 +119,5 @@ madx.exit()
 #    The use of the following functions, methods, classes and modules is shown
 #    in this example:
 #
-#    - `~.cpymadtools.lhc`: `~.lhc._setup.make_lhc_beams`
-#    - `~.plotting.aperture`: `~.plotting.aperture.plot_aperture`
+#    - `~.cpymadtools.lhc`: `~.lhc.prepare_lhc_run3`
+#    - `~.plotting.aperture`: `~.plotting.aperture.plot_aperture`, `~.plotting.aperture.plot_physical_apertures`
