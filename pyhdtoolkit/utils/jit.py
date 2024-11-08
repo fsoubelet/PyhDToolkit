303b6c0487b1c4701d3abd862248a517c339c079
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-08 15:36:44 +0100

    add jit decorator in new utils module

---
+++ pyhdtoolkit/utils/jit.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Callable
+
+
+def maybe_jit(func: Callable, **kwargs) -> Callable:
+    """
+    A `numba.jit` decorator that does nothing if `numba` is not installed.
+
+    Args:
+        func (Callable): The function to be decorated.
+        **kwargs: Additional keyword arguments are passed to `numba.jit`.
+
+    Returns:
+        Callable: The JIT-decorated function if `numba` is installed,
+            and the original function otherwise.
+    """
+    try:
+        from numba import jit
+
+        return jit(func, **kwargs)
+    except ImportError:
+        return func
