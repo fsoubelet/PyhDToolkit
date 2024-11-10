0e9cff1bb87af5df7c1b31bbc97abfd8ef8fa738
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-10 09:58:37 +0100

    single line return with type

--- docs/conf.py
+++ docs/conf.py
@@ -149,6 +149,7 @@ napoleon_include_init_with_doc = True
 napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
+napoleon_use_rtype = False
 napoleon_preprocess_types = True
 napoleon_attr_annotations = True
 
