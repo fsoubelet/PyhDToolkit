e89d58b5731063d61adcb05da29b53d1fd0f2443
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-12 17:18:26 +0100

    numpy style

--- pyhdtoolkit/optics/ripken.py
+++ pyhdtoolkit/optics/ripken.py
@@ -4,7 +4,8 @@
 Ripken Parameters
 -----------------
 
-Module implementing various calculations based on the :cite:t:`Ripken:optics:1989` optics parameters.
+Module implementing various calculations based
+on the :cite:t:`Ripken:optics:1989` optics parameters.
 """
 
 from __future__ import annotations
@@ -26,25 +27,35 @@ def lebedev_beam_size(
     """
     .. versionadded:: 0.8.2
 
-    Calculate beam size according to the Lebedev-Bogacz formula, based on the Ripken-Mais Twiss
-    parameters. The implementation is that of Eq. (A.3.1) in :cite:t:`Lebedev:coupling:2010`.
+    Calculate beam size according to the Lebedev-Bogacz formula, based
+    on the Ripken-Mais Twiss parameters. The implementation is that of
+    Eq. (A.3.1) in :cite:t:`Lebedev:coupling:2010`.
 
-    .. tip::
-        For the calculations, use :math:`\\beta_{11}` and :math:`\\beta_{21}` for the **vertical**
-        beam size, but use :math:`\\beta_{12}` and :math:`\\beta_{22}` for the **horizontal** one.
-        See the example below.
+    Hint
+    ----
+        For the calculations, use :math:`\\beta_{11}` and :math:`\\beta_{21}`
+        for the **vertical** beam size, but use :math:`\\beta_{12}` and
+        :math:`\\beta_{22}` for the **horizontal** one. See the example below.
 
-    Args:
-        beta1_ (float | np.ndarray): value(s) for the beta1x or beta1y Ripken parameter.
-        beta2_ (float | np.ndarray): value(s) for the beta2x or beta2y Ripken parameter.
-        geom_emit_x (float): geometric emittance of the horizontal plane, in [m].
-        geom_emit_y (float): geometric emittante of the vertical plane, in [m].
+    Parameters
+    ----------
+    beta1_ : float | numpy.ndarray
+        Value(s) for the beta1x or beta1y Ripken parameters.
+    beta2_ : float | numpy.ndarray
+        Value(s) for the beta2x or beta2y Ripken parameters.
+    geom_emit_x : float
+        Geometric emittance of the horizontal plane, in [m].
+    geom_emit_y : float
+        Geometric emittante of the vertical plane, in [m].
 
-    Returns:
-        The beam size (horizontal or vertical) according to Lebedev & Bogacz, as
+    Returns
+    -------
+    float | numpy.ndarray
+        The beam size (horizontal or vertical) according to Lebedev& Bogacz, as
         :math:`\\sqrt{\\epsilon_x * \\beta_{1,\\_}^2 + \\epsilon_y * \\beta_{2,\\_}^2}`.
 
-    Example:
+    Example
+    -------
         .. code-block:: python
 
             geom_emit_x = madx.globals["geometric_emit_x"]
@@ -65,19 +76,27 @@ def lebedev_beam_size(
 
 def _beam_size(coordinates_distribution: np.ndarray, method: str = "std") -> float:
     """
-    Computes the beam size from particle coordinates, either as the standard deviation
-    or as the root mean square of the distribution.
+    Computes the beam size from particle coordinates, either as the
+    standard deviation or as the root mean square of the distribution.
 
-    Args:
-        coordinates_distribution (np.ndarray): ensemble of coordinates of the particle distributon.
-        method (str): the method of calculation to use, either 'std' (using the standard deviation as the
-            beam size) or 'rms' (root mean square). Case-insensitive.
+    Parameters
+    ----------
+    coordinates_distribution : numpy.ndarray
+        Ensemble of coordinates of the particle distribution.
+    method : str
+        The method of calculation to use, either 'std' (using the standard
+        deviation as the beam size) or 'rms' (root mean square). Case
+        insensitive.
 
-    Returns:
+    Returns
+    -------
+    float
         The computed beam size.
 
-    Raises:
-        NotImplementedError: If the required *method* is neither std nor rms.
+    Raises
+    ------
+    NotImplementedError
+        If the required *method* is neither std nor rms.
     """
     if method.lower() not in ("std", "rms"):
         msg = "Invalid method provided"
@@ -89,9 +108,10 @@ def _beam_size(coordinates_distribution: np.ndarray, method: str = "std") -> flo
 
 def _add_beam_size_to_df(df: TfsDataFrame, geom_emit_x: float, geom_emit_y: float) -> TfsDataFrame:
     """
-    Adds columns with the horizontal and vertical Lebedev beam sizes to a dataframe
-    that already contains Ripken Twiss parameters. Assumes that the geometrical emittance
-    is identical for the horizontal and vertical plane, which is something I usually have.
+    Adds columns with the horizontal and vertical Lebedev beam sizes
+    to a dataframe that already contains Ripken Twiss parameters.
+    Assumes that the geometrical emittance is identical for the horizontal
+    and vertical plane, which is something I usually have. Beware.
     """
     res = df.copy(deep=True)
     res["SIZE_X"] = lebedev_beam_size(res.BETA11, res.BETA21, geom_emit_x, geom_emit_y)  # horizontal
