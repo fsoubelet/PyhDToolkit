95c1a449513f1763adc111a4a85d1f54d52210bd
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-12 16:55:06 +0100

    numpy style

--- pyhdtoolkit/plotting/envelope.py
+++ pyhdtoolkit/plotting/envelope.py
@@ -35,43 +35,55 @@ def plot_beam_envelope(
     """
     .. versionadded:: 1.2.0
 
-    Draws the beam enveloppe around the beam orbit on the given *axis*.
-    The enveloppe is determined from the active sequence's beam's parameters.
+    Draws the beam enveloppe around the beam orbit on the given
+    *axis*. The enveloppe is determined from the active sequence's
+    beam's parameters.
 
-    One can find an example use of this function in the
-    :ref:`beam enveloppe <demo-beam-enveloppe>` example gallery.
+    One can find an example use of this function in the :ref:`beam
+    enveloppe <demo-beam-enveloppe>` example gallery.
 
-    Args:
-        madx (cpymad.madx.Madx): an instanciated `~cpymad.madx.Madx` object.
-            Positional only.
-        sequence (str): the name of the sequence to plot the beam enveloppe
-            for, should be the active sequence. Case-insensitive.
-        plane (str): the physical plane to plot for, should be either `x`,
-            `horizontal`, `y` or `vertical`, and is case-insensitive.
-        nsigma (float): the standard deviation to use for the beam enveloppe
-            calculation. A value of 3 will draw the 3 sigma beam enveloppe.
-            Defaults to 1.
-        scale (float): a scaling factor to apply to the beam orbit and beam
-            enveloppe, for the user to adjust to their wanted scale. Defaults
-            to 1 (values in [m]).
-        xoffset (float): An offset applied to the ``S`` coordinate before
-            plotting. This is useful if you want to center a plot around a
-            specific point or element, which would then become located
-            at :math:`s = 0`. Beware this offset is applied before applying
-            the *xlimits*. Defaults to 0.
-        xlimits (tuple[float, float]): will implement xlim (for the ``s``
-            coordinate) if this is not ``None``, using the tuple passed.
-            Defaults to ``None``.
-        **kwargs: any keyword argument that can be given to the ``MAD-X``
-            ``TWISS`` command. If either `ax` or `axis` is found in the
-            kwargs, the corresponding value is used as the axis object to
-            plot on.
+    Parameters
+    ----------
+    madx : cpymad.madx.Madx
+        An instanciated `~cpymad.madx.Madx` object. Positional only.
+    sequence : str
+        The name of the sequence to plot the beam enveloppe for, which
+        should be the active sequence. Case insensitive.
+    plane : str
+        The physical plane to plot for, which should be either `x`,
+        `horizontal`, `y` or `vertical`. Case insensitive.
+    nsigma : float
+        The standard deviation to use for the beam enveloppe calculation.
+        For instance, providing a value of 3 will draw the 3 sigma beam
+        enveloppe. Defaults to 1.
+    scale : float
+        A scaling factor to apply to the beam orbit and beam enveloppe,
+        for the user to adjust to their wanted scale. Defaults to 1
+        (meaning values are assumed in [m]).
+    xoffset : float
+        An offset applied to the ``S`` coordinate before plotting. This
+        is useful if you want to center a plot around a specific point
+        or element, which would then become located at :math:`s = 0`.
+        Beware this offset is applied before applying the *xlimits*.
+        Defaults to 0.
+    xlimits : tuple[float, float], optional
+        If given, will be used for the xlim (for the ``s`` coordinate),
+        using the tuple passed.
+    **kwargs
+        Any keyword argument that can be given to the ``MAD-X``
+        ``TWISS`` command. If either `ax` or `axis` is found in the
+        kwargs, the corresponding value is used as the axis object to
+        plot on.
 
-    Raises:
-        ValueError: if the *plane* argument is not one of `x`, `horizontal`,
+    Raises
+    ------
+    ValueError
+        If the *plane* argument is not one of `x`, `horizontal`,
         `y` or `vertical`.
 
-    Examples:
+    Examples
+    --------
+
         .. code-block:: python
 
             fig, ax = plt.subplots(figsize=(10, 9))
@@ -133,7 +145,10 @@ def plot_beam_envelope(
 
 
 def _interpolate_madx(madx: Madx, /) -> None:
-    """Run interpolation on the provided MAD-X instance with default slice values."""
+    """
+    Run interpolation on the provided MAD-X
+    instance with default slice values.
+    """
     logger.debug("Running interpolation in MAD-X")
     madx.command.select(flag="interpolate", class_="drift", slice_=4, range_="#s/#e")
     madx.command.select(flag="interpolate", class_="quadrupole", slice_=8, range_="#s/#e")
