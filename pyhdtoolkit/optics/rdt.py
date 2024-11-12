ead090da606f7bf0d76f878ab2628b9ba8c17687
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-12 16:05:25 +0100

    numpy style, add versionadded and add examples

--- pyhdtoolkit/optics/rdt.py
+++ pyhdtoolkit/optics/rdt.py
@@ -4,21 +4,41 @@
 Resonance Driving Terms Utilities
 ---------------------------------
 
-Module implementing utilities for the handling of resonance driving terms.
+Module implementing utilities for the handling of
+resonance driving terms.
 """
 
 
 def rdt_to_order_and_type(rdt: int | str) -> str:
     """
+    .. versionadded:: 1.5.0
+
     Decompose the input RDT into its four various components
     and return the type of RDT (normal or skew) and its order.
 
-    Args:
-        rdt (int | str): the RDT to decompose.
+    Parameters
+    ----------
+    rdt : int | str
+        The RDT to decompose.
 
-    Returns:
-        A string with the type and (magnet) order of
+    Returns
+    -------
+    str
+        A string with the type and magnet order of
         the provided RDT.
+    
+    Examples
+    --------
+
+        .. code-block:: python
+
+            rdt_to_order_and_type(1001)
+            # Output: 'skew_quadrupole'
+        
+        .. code-block:: python
+
+            rdt_to_order_and_type("2002")
+            # Output: 'normal_octupole'
     """
     j, k, l, m = map(int, str(rdt))  # noqa: E741
     rdt_type = "normal" if (l + m) % 2 == 0 else "skew"
@@ -37,18 +57,40 @@ def rdt_to_order_and_type(rdt: int | str) -> str:
 
 def determine_rdt_line(rdt: int | str, plane: str) -> tuple[int, int, int]:
     """
-    Find the given line to look for in the spectral analysis of
-    the given plane that corresponds to the given RDT.
+    .. versionadded:: 1.5.0
 
-    Args:
-        rdt (int | str): the RDT to look for.
-        plane (str): the plane to look for the RDT in.
+    Find the given line to look for in the spectral analysis
+    of the given plane that corresponds to the given RDT.
 
-    Returns:
-        A tuple of three integers representing the line
-        to look for in the spectral analysis. For instance,
-        f1001 corresponds to the line (0, 1, 0) in the X plane
-        which means the line located at 1 * Qy = Qy.
+    Parameters
+    ----------
+    rdt : int | str
+        The RDT to look for.
+    plane : str
+        The plane to look for the RDT in.
+
+    Returns
+    -------
+    tuple[int, int, int]
+        A tuple of three integers representing the line to
+        look for in the spectral analysis. For instance,
+        f1001 corresponds to the line (0, 1, 0) in the X
+        plane which means the line located at 1 * Qy = Qy.
+
+    Examples
+    --------
+
+        .. code-block:: python
+
+            determine_rdt_line(1001, "X")
+            # Output: (0, 1, 0)
+            # Line at 1 * Qy in the X spectrum.
+        
+        .. code-block:: python
+
+            determine_rdt_line("2002", "Y")
+            # Output: (-2, 3, 0)
+            # Line at 3 * Qy - 2 * Qx in the Y spectrum.
     """
     j, k, l, m = map(int, str(rdt))  # noqa: E741
     lines = {"X": (1 - j + k, m - l, 0), "Y": (k - j, 1 - l + m, 0)}
