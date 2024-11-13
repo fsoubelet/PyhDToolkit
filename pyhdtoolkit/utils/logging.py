9f969b5c300b1ebcc4628ef949b24d12960fd84e
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-13 15:52:43 +0100

    numpy style

--- pyhdtoolkit/utils/logging.py
+++ pyhdtoolkit/utils/logging.py
@@ -7,8 +7,10 @@ Logging Utilities
 
 .. versionadded:: 1.0.0
 
-The `loguru` package is used for logging throughout `pyhdtoolkit`, and this module provides utilities to easily set up a logger configuration.
-Different pre-defined formats are provided to choose from:
+The `loguru` package is used for logging throughout `pyhdtoolkit`,
+and this module provides utilities to easily set up a logger
+configuration. Different pre-defined formats are provided to choose
+from:
 
 - ``FORMAT1``: will display the full time of the log message, its level, the calling line and the message itself. This is the default format.
 - ``FORMAT2``: similar to ``FORMAT1``, but the caller information is displayed at the end of the log line.
@@ -40,19 +42,27 @@ def config_logger(level: str | int = "INFO", fmt: str = FORMAT1, **kwargs) -> No
     """
     .. versionadded:: 0.8.2
 
-    Resets the logger object from `loguru`, with `sys.stdout` as a sink and the
-    provided *format*.
+    Resets the logger object from `loguru`, with `sys.stdout`
+    as a sink and the provided *format*.
 
-    Args:
-        level (Union[str, int]): the logging level to set. Case-insensitive if a
-            string is given. Can be any of the
-            `loguru levels <https://loguru.readthedocs.io/en/stable/api/logger.html#levels>`_
-            or their integer values equivalents. Defaults to ``INFO``.
-        fmt (str): the format to use for the logger to display messages. Defaults
-            to a pre-defined format in this module.
-        **kwargs: any keyword argument is transmitted to the `~loguru._logger.Logger.add` call.
+    Parameters
+    ----------
+    level : str | int
+        The logging level to set. Case insensitive if a string
+        is given. Valuevalue can be any of the `loguru levels
+        <https://loguru.readthedocs.io/en/stable/api/logger.html#levels>`_
+        or their integer values equivalents. Defaults to ``INFO``.
+    fmt : str
+        The format to use for the logger to display messages.
+        Defaults to a pre-defined format in this module.
+    **kwargs
+        Any keyword argument is transmitted to the
+        `~loguru._logger.Logger.add` call.
 
-    Example:
+
+    Examples
+    --------
+
         Using the defaults and setting the logging level:
 
         .. code-block:: python
