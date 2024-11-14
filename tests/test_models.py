dd1d60952f5c2d721a375562fcefe8a1d1995940
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-14 12:41:32 +0100

    also gemitt_x

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,7 +8,7 @@ def test_beam_parameters():
         charge=1,
         pc_GeV=1.9,
         E_0_GeV=0.9382720813,
-        nemitt_x=5e-6,
+        nemitt_x=3e-6,
         nemitt_y=5e-6,
         deltap_p=2e-3,
         verbose=True,
@@ -18,7 +18,8 @@ def test_beam_parameters():
     assert math.isclose(beam.E_kin_GeV, 1.1807735761946736)
     assert math.isclose(beam.gamma_rel, 2.258455409393277)
     assert math.isclose(beam.beta_rel, 0.8966300434726596)
-    assert math.isclose(beam.nemitt_x, 5e-06)
+    assert math.isclose(beam.nemitt_x, 3e-06)
+    assert math.isclose(beam.gemitt_x, 1.1910308785510202e-06)
     assert math.isclose(beam.gemitt_y, 1.9850514642517005e-06)
     beam.__str__()
     beam.__repr__()
