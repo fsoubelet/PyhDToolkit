4a67b881f097f7b00ff443e122bea5e7cd5567a4
Felix Soubelet <felix.soubelet@protonmail.com>
2026-01-08 12:48:42 +0100

    extracted

--- pyhdtoolkit/scripts/htc_monitor.py
+++ pyhdtoolkit/scripts/htc_monitor.py
@@ -26,45 +26,51 @@ from pyhdtoolkit.utils.logging import config_logger
 
 config_logger(level="ERROR")
 
+# ----- Bread and Butter ----- #
+
+def generate_renderable() -> Group:
+    """
+    .. versionadded:: 0.9.0
+
+    Function called to update the live display,
+    fetches data from htcondor via 'condor_q',
+    processes the response into tables and returns
+    a `rich.console.Group` with Panels for both
+    the tasks and cluster information.
+
+    Returns
+    -------
+    rich.console.Group
+        A `rich.console.Group` object with two
+        `rich.panel.Panel` objects inside, one
+        holding the tasks table and the other
+        holding the cluster information.
+    """
+    condor_string = query_condor_q()
+    user_tasks, cluster_info = read_condor_q(condor_string)
+    owner = user_tasks[0].owner if user_tasks else "User"
+
+    tasks_table = _make_tasks_table(user_tasks)
+    cluster_table = _make_cluster_table(owner, cluster_info)
+    return Group(
+        Panel(
+            tasks_table,
+            title=f"Scheduler: {cluster_info.scheduler_id}.cern.ch",
+            expand=False,
+            border_style="scope.border",
+        ),
+        Panel(
+            cluster_table,
+            title=f"{cluster_info.scheduler_id} Statistics",
+            expand=False,
+            border_style="scope.border",
+        ),
+    )
+
+# ----- Entrypoint ----- #
+
 @logger.catch()
 def main():
-    def generate_renderable() -> Group:
-        """
-        .. versionadded:: 0.9.0
-
-        Function called to update the live display,
-        fetches data from htcondor, does the processing
-        and returns a Group with both Panels.
-
-        Returns
-        -------
-        rich.console.Group
-            A `rich.console.Group` object with two
-            `rich.panel.Panel` objects inside, one
-            holding the tasks table and the other
-            holding the cluster information.
-        """
-        condor_string = query_condor_q()
-        user_tasks, cluster_info = read_condor_q(condor_string)
-        owner = user_tasks[0].owner if user_tasks else "User"
-
-        tasks_table = _make_tasks_table(user_tasks)
-        cluster_table = _make_cluster_table(owner, cluster_info)
-        return Group(
-            Panel(
-                tasks_table,
-                title=f"Scheduler: {cluster_info.scheduler_id}.cern.ch",
-                expand=False,
-                border_style="scope.border",
-            ),
-            Panel(
-                cluster_table,
-                title=f"{cluster_info.scheduler_id} Statistics",
-                expand=False,
-                border_style="scope.border",
-            ),
-        )
-
     with Live(generate_renderable(), refresh_per_second=0.25) as live:
         live.console.log("Querying HTCondor Queue - Refreshed Every 5 Minutes\n")
         while True:
@@ -75,6 +81,7 @@ def main():
                 live.console.log("Exiting Program")
                 break
 
+# ----- Script Mode ----- #
 
 if __name__ == "__main__":
     main()
